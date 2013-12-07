972db9c7d66aad4fd0af0a01ee8f63eb83697dbc
Wim Godden <wim@wimgodden.be>
2013-12-07 04:52:31 +0100

    a

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,13 +9,17 @@ if (defined('PHP_CODESNIFFER_IN_TESTS') === false) {
     define('PHP_CODESNIFFER_IN_TESTS', true);
 }
 
-$vendorDir = __DIR__ . '/../vendor';
 
-if (!@include($vendorDir . '/autoload.php')) {
-    die("You must set up the project dependencies, run the following commands:
-wget http://getcomposer.org/composer.phar
-php composer.phar install
-");
+defined('TRAVIS_CI_TEST') || define('TRAVIS_CI_TEST', 0);
+
+if (TRAVIS_CI_TEST == 1) {
+    $vendorDir = __DIR__ . '/../../../../../';
+    if (!@include($vendorDir . '/autoload.php')) {
+        die("You must set up the project dependencies, run the following commands:
+    wget http://getcomposer.org/composer.phar
+    php composer.phar install
+    ");
+    }
 }
 
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'BaseSniffTest.php';
