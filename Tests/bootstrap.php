2e782c720eae3c34cd59485326f96273d14d23e5
Wim Godden <wim@wimgodden.be>
2013-12-07 04:40:33 +0100

    a

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,7 +9,14 @@ if (defined('PHP_CODESNIFFER_IN_TESTS') === false) {
     define('PHP_CODESNIFFER_IN_TESTS', true);
 }
 
-require_once 'PHP/CodeSniffer/CLI.php';
+$vendorDir = __DIR__ . '/../vendor';
+
+if (!@include($vendorDir . '/.composer/autoload.php')) {
+    die("You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+");
+}
 
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'BaseSniffTest.php';
 
