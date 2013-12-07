59e660e3aac6a6950ac443465aaedc5ac70d6a2c
Wim Godden <wim@wimgodden.be>
2013-12-07 04:27:17 +0100

    Changes for Travis CI

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -9,5 +9,7 @@ if (defined('PHP_CODESNIFFER_IN_TESTS') === false) {
     define('PHP_CODESNIFFER_IN_TESTS', true);
 }
 
+require_once 'PHP/CodeSniffer/CLI.php';
+
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'BaseSniffTest.php';
 
