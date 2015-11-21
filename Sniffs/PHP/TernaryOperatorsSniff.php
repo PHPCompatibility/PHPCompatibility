dfbe2785e10bc5ea4dca5651b1d5f85e349e9e38
Mark Clements <github@kennel17.co.uk>
2015-11-21 00:01:16 +0000

    Sniff for missing middle of ternary operators in PHP < 5.3.
    Unit tests included.
    
    Fixed #49.

---
+++ Sniffs/PHP/TernaryOperatorsSniff.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * PHPCompatibility_Sniffs_PHP_TernaryOperatorsSniff.
+ *
+ * PHP version 5.3
+ *
+ * @category  PHP
+ * @package   PHPCompatibility
+ * @author    Ben Selby <bselby@plus.net>
+ * @copyright 2012 Ben Selby
+ */
+
+/**
+ * PHPCompatibility_Sniffs_PHP_TernaryOperatorsSniff.
+ *
+ * Performs checks on ternary operators, specifically that the middle expression
+ * is not omitted for versions that don't support this.
+ *
+ * PHP version 5.3
+ *
+ * @category  PHP
+ * @package   PHPCompatibility
+ * @author    Ben Selby <bselby@plus.net>
+ * @copyright 2012 Ben Selby
+ */
+class PHPCompatibility_Sniffs_PHP_TernaryOperatorsSniff extends PHPCompatibility_Sniff
+{
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_INLINE_THEN);
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in the
+     *                                        stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        if ($this->supportsAbove('5.3')) {
+            $tokens = $phpcsFile->getTokens();
+        
+            // Get next non-whitespace token, and check it isn't the related inline else
+            // symbol, which is not allowed prior to PHP 5.3.
+            $next = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens,
+                                         ($stackPtr + 1), null, true);
+
+            if ($tokens[$next]['code'] == T_INLINE_ELSE) {
+                $error = sprintf(
+                    "Middle may not be omitted from ternary operators in PHP < 5.3"
+                );
+                $phpcsFile->addWarning($error, $stackPtr);
+            }
+        }
+    }
+}
