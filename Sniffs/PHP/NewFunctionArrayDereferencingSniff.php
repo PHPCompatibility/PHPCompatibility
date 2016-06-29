eea2a5f9af8003c5b806e5ccdd3a70423feda649
wimg <wim@wimgodden.be>
2016-06-29 17:10:25 +0200

    Fixes #52

---
+++ Sniffs/PHP/NewFunctionArrayDereferencingSniff.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * PHPCompatibility_Sniffs_PHP_NewFunctionArrayDereferencingSniff.
+ *
+ * @category  PHP
+ * @package   PHPCompatibility
+ * @author    Wim Godden <wim.godden@cu.be>
+ */
+
+/**
+ * PHPCompatibility_Sniffs_PHP_NewFunctionArrayDereferencingSniff.
+ *
+ * @category  PHP
+ * @package   PHPCompatibility
+ * @author    Wim Godden <wim.godden@cu.be>
+ */
+class PHPCompatibility_Sniffs_PHP_NewFunctionArrayDereferencingSniff extends PHPCompatibility_Sniff
+{
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_STRING);
+    }//end register()
+    
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        $ignore = array(
+                T_DOUBLE_COLON,
+                T_OBJECT_OPERATOR,
+                T_FUNCTION,
+                T_CONST,
+        );
+
+        $prevToken = $phpcsFile->findPrevious(T_WHITESPACE, ($stackPtr - 1), null, true);
+        if (in_array($tokens[$prevToken]['code'], $ignore) === true) {
+            // Not a call to a PHP function.
+            return;
+        }
+        
+        if (isset($tokens[$stackPtr + 1]) && $tokens[$stackPtr + 1]['type'] == 'T_OPEN_PARENTHESIS') {
+            $closeParenthesis = $tokens[$stackPtr + 1]['parenthesis_closer'];
+            if ($tokens[$closeParenthesis + 1]['type'] == 'T_OPEN_SQUARE_BRACKET') {
+                if ($this->supportsBelow('5.3')) {
+                    $phpcsFile->addError('Function array dereferencing is not present in PHP version 5.3 or earlier', $stackPtr + 3);
+                }
+            }
+        }
+    }//end process()
+}//end class
