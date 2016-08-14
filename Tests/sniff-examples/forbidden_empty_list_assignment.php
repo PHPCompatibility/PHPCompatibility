75b8212b93218b2c84ea0caaf8b8f09fae13ceb6
jrfnl <github_nospam@adviesenzo.nl>
2016-08-14 09:58:00 +0200

    Improve `ForbiddenEmptyListAssignment` sniff (code review).
    * Trigger error when there are only comments between the parenthesis.
    * Trigger error when there are empty parenthesis between the list parenthesis.
    * Don't trigger error for unfinished code when review is used during live coding (in an editor).
    * Slightly simplify the code.
    * Remove unnecessary `sprintf()`.
    
    Includes unit tests demonstrating the issue with the comments.
    Includes some extra unit tests for valid list assignments.
    
    As there are now more test cases, refactored the unit tests to use data providers as well.

--- Tests/sniff-examples/forbidden_empty_list_assignment.php
+++ Tests/sniff-examples/forbidden_empty_list_assignment.php
@@ -1,5 +1,20 @@
 <?php
 
 list() = 5;
-
 list(, ,) = 5;
+list(/*comment*/) = 5;
+list( /*comment*/ /*another comment*/ ) = 5;
+list(,(),) = 5;
+
+
+/*
+ * The below list assignments are all valid.
+ */
+list( $item, $anotherItem ) = $infoArray;
+list($drink, , $power) = $infoArray;
+list(, , $power) = $infoArray;
+list($a[0], $a[1], $a[2]) = $infoArray;
+list( ${$drink} ) = $infoArray;
+
+// Don't trigger on unfinished code during live code review.
+list(
