742b27e08cf8cf8ff437dfc6ff4ddc5e776e4c45
jrfnl <github_nospam@adviesenzo.nl>
2016-08-20 14:15:55 +0200

    Fix `TernaryOperators` sniff (code review).
    * The sniff was not reporting errors for ranges which includes 5.2 or lower, but also included 5.3  or higher. Logic for version testing was using the wrong method.
    * Remove unnecessary `sprintf()`
    
    * Added additional unit test - code taken from this sniff library which was not being reported which was the reason I started looking into this one.

--- Sniffs/PHP/TernaryOperatorsSniff.php
+++ Sniffs/PHP/TernaryOperatorsSniff.php
@@ -47,20 +47,20 @@ class PHPCompatibility_Sniffs_PHP_TernaryOperatorsSniff extends PHPCompatibility
      */
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
     {
-        if (!$this->supportsAbove('5.3')) {
-            $tokens = $phpcsFile->getTokens();
+        if ($this->supportsBelow('5.2') === false) {
+            return;
+        }
 
-            // Get next non-whitespace token, and check it isn't the related inline else
-            // symbol, which is not allowed prior to PHP 5.3.
-            $next = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens,
-                                         ($stackPtr + 1), null, true);
+        $tokens = $phpcsFile->getTokens();
 
-            if ($tokens[$next]['code'] == T_INLINE_ELSE) {
-                $error = sprintf(
-                    "Middle may not be omitted from ternary operators in PHP < 5.3"
-                );
-                $phpcsFile->addWarning($error, $stackPtr);
-            }
+        // Get next non-whitespace token, and check it isn't the related inline else
+        // symbol, which is not allowed prior to PHP 5.3.
+        $next = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens,
+                                     ($stackPtr + 1), null, true);
+
+        if ($next !== false && $tokens[$next]['code'] === T_INLINE_ELSE) {
+            $error = 'Middle may not be omitted from ternary operators in PHP < 5.3';
+            $phpcsFile->addWarning($error, $stackPtr);
         }
     }
 }
