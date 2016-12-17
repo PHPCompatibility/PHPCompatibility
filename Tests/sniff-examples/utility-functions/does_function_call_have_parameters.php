885f38e1fa684364f9bf8a5f9159619f8ceaa307
jrfnl <github_nospam@adviesenzo.nl>
2016-12-17 20:37:56 +0100

    Allow the function parameter utility functions to also work when with arrays.
    Adjusted to work with both short as well as long arrays.
    
    Includes additional unit tests & fixed documentation for the unit tests.

--- Tests/sniff-examples/utility-functions/does_function_call_have_parameters.php
+++ Tests/sniff-examples/utility-functions/does_function_call_have_parameters.php
@@ -19,3 +19,29 @@ some_function(/*nothing here*/);
 some_function( 1 );
 some_function(1,2,3);
 some_function(true);
+
+/*
+ * Even though a language construct and not a function call, the function should work just as well for arrays.
+ */
+
+/*
+ * No parameters.
+ */
+$foo = array();
+$foo = array(     );
+$foo = array( /*nothing here*/ );
+$foo = array(/*nothing here*/);
+$bar = [];
+$bar = [     ];
+$bar = [ /*nothing here*/ ];
+$bar = [/*nothing here*/];
+
+/*
+ * Has parameters.
+ */
+$foo = array( 1 );
+$foo = array(1,2,3);
+$foo = array(true);
+$bar = [ 1 ];
+$bar = [1,2,3];
+$bar = [true];
