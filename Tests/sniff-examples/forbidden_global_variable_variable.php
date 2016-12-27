aaa22319f33593eb96aa34baea68671adeb5c0d0
jrfnl <github_nospam@adviesenzo.nl>
2016-12-27 09:31:35 +0100

    Allow for multiple variables made global in ForbiddenGlobalVariableVariableSniff
    The ForbiddenGlobalVariableVariableSniff would currently only detect variable variables in a global statement if it was the first variable of the statement.
    It basically didn't take multi-variable global statements into account.
    
    Includes additional unit tests.
    Includes reworking the test class to use data providers as there are now more test cases.

--- Tests/sniff-examples/forbidden_global_variable_variable.php
+++ Tests/sniff-examples/forbidden_global_variable_variable.php
@@ -1,3 +1,28 @@
 <?php
 
+/*
+ * Forbidden global variable variables.
+ */
 global $$test;
+
+// Multi-variable and multi-line global statements.
+global $test, $$test, $$$test;
+
+global $test,
+    $$obj->$bar,
+    $testing,
+    $$test;
+
+/*
+ * Ok.
+ */
+global $test;
+global ${$test};
+global ${"name"};
+global ${"name_$type"};
+global ${$var['key1']['key2']};
+global ${$obj->$bar};
+global ${$obj->{$var['key']}};
+
+// Live coding.
+global
