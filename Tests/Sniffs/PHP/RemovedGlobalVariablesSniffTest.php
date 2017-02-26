eb73f2a1dde8310c49a94dc4536eafe8f8784cb9
jrfnl <github_nospam@adviesenzo.nl>
2017-02-26 09:52:05 +0100

    Merge RemovedGlobalVariables sniff with the LongArraysSniff
    Basically both sniffs were checking for removed predefined global PHP variables, so it seemed strange that the checks were divided over two sniffs.
    
    Notes:
    * The `RemovedGlobalVariables` sniff was already set up to use a complex version array and had the more intuïtive file name, which is why I choose to merge both into that one.
    * Using the logic already there, also allowed for adding an `alternative` suggestion.
    * The `LongArray` sniff however contained logic to detect class properties shadowing removed superglobals. This logic has been moved over to the `RemovedGlobalVariables` sniff, making it also available to the variable which was already being detected by that sniff.
    * Unit test files have also been merged and the test method made more flexible.
    
    N.B.: I've run the new sniff over the old `long_arrays.php` test case file and - apart from the message text change -, the results are the same.

--- Tests/Sniffs/PHP/RemovedGlobalVariablesSniffTest.php
+++ Tests/Sniffs/PHP/RemovedGlobalVariablesSniffTest.php
@@ -27,21 +27,33 @@ class RemovedGlobalVariablesSniffTest extends BaseSniffTest
      *
      * @dataProvider dataRemovedGlobalVariables
      *
-     * @param string $varName The name of the removed global variable.
-     * @param int    $line    The line number where the error is expected.
+     * @param string $varName      The name of the removed global variable.
+     * @param string $deprecatedIn The PHP version in which the global variable was deprecated.
+     * @param string $removedIn    The PHP version in which the global variable was removed.
+     * @param array  $lines        The line numbers in the test file which apply to this variable.
+     * @param string $alternative  What to use as an alternative.
+     * @param string $okVersion    A PHP version in which the global variable was ok to be used.
      *
      * @return void
      */
-    public function testRemovedGlobalVariables($varName, $line)
+    public function testRemovedGlobalVariables($varName, $deprecatedIn, $removedIn, $lines, $alternative, $okVersion)
     {
-        $file = $this->sniffFile(self::TEST_FILE, '5.5');
-        $this->assertNoViolation($file, $line);
+        $file = $this->sniffFile(self::TEST_FILE, $okVersion);
+        foreach ($lines as $line) {
+            $this->assertNoViolation($file, $line);
+        }
 
-        $file = $this->sniffFile(self::TEST_FILE, '5.6');
-        $this->assertWarning($file, $line, "Global variable '$" . $varName . "' is deprecated since PHP 5.6; Use php://input instead");
+        $file  = $this->sniffFile(self::TEST_FILE, $deprecatedIn);
+        $error = "Global variable '$" . $varName . "' is deprecated since PHP {$deprecatedIn}; Use {$alternative} instead";
+        foreach ($lines as $line) {
+            $this->assertWarning($file, $line, $error);
+        }
 
-        $file = $this->sniffFile(self::TEST_FILE, '7.0');
-        $this->assertError($file, $line, "Global variable '$" . $varName . "' is deprecated since PHP 5.6 and removed since PHP 7.0; Use php://input instead");
+        $file  = $this->sniffFile(self::TEST_FILE, $removedIn);
+        $error = "Global variable '$" . $varName . "' is deprecated since PHP {$deprecatedIn} and removed since PHP {$removedIn}; Use {$alternative} instead";
+        foreach ($lines as $line) {
+            $this->assertError($file, $line, $error);
+        }
     }
 
     /**
@@ -54,7 +66,15 @@ class RemovedGlobalVariablesSniffTest extends BaseSniffTest
     public function dataRemovedGlobalVariables()
     {
         return array(
-            array('HTTP_RAW_POST_DATA', 3),
+            array('HTTP_POST_VARS', '5.3', '5.4', array(9, 31), '$_POST', '5.2'),
+            array('HTTP_GET_VARS', '5.3', '5.4', array(10, 32, 51), '$_GET', '5.2'),
+            array('HTTP_ENV_VARS', '5.3', '5.4', array(11, 33, 52), '$_ENV', '5.2'),
+            array('HTTP_SERVER_VARS', '5.3', '5.4', array(12, 34), '$_SERVER', '5.2'),
+            array('HTTP_COOKIE_VARS', '5.3', '5.4', array(13, 35), '$_COOKIE', '5.2'),
+            array('HTTP_SESSION_VARS', '5.3', '5.4', array(14, 36), '$_SESSION', '5.2'),
+            array('HTTP_POST_FILES', '5.3', '5.4', array(15, 37), '$_FILES', '5.2'),
+
+            array('HTTP_RAW_POST_DATA', '5.6', '7.0', array(3, 38, 53), 'php://input', '5.5'),
         );
     }
 
@@ -70,7 +90,7 @@ class RemovedGlobalVariablesSniffTest extends BaseSniffTest
      */
     public function testNoFalsePositives($line)
     {
-        $file = $this->sniffFile(self::TEST_FILE, '7.0');
+        $file = $this->sniffFile(self::TEST_FILE, '99.0');
         $this->assertNoViolation($file, $line);
     }
 
@@ -84,8 +104,29 @@ class RemovedGlobalVariablesSniffTest extends BaseSniffTest
     public function dataNoFalsePositives()
     {
         return array(
+            // Variable names are case-sensitive.
             array(5),
             array(6),
+
+            // Issue #268 - class properties named after long array variables.
+            array(20),
+            array(21),
+            array(22),
+            array(23),
+            array(24),
+            array(25),
+            array(26),
+            array(27),
+
+            array(41),
+            array(42),
+            array(43),
+            array(44),
+            array(45),
+            array(46),
+            array(47),
+            array(48),
         );
     }
+
 }
