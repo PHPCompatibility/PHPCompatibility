9252753a36a3976eb8474daa6a7f016194b51397
jrfnl <github_nospam@adviesenzo.nl>
2017-04-03 09:01:42 +0200

    NewClosures sniff: add checking for new features added in PHP 5.4
    Closures were introduced in PHP 5.3.
    However, until PHP 5.4, they could not be declared as `static`, nor could they use the `$this` variable when declared within a class context.
    Ref: http://php.net/manual/en/functions.anonymous.php
    
    This will now be sniffed for.
    
    Includes unit tests.

--- Tests/Sniffs/PHP/NewClosureSniffTest.php
+++ Tests/Sniffs/PHP/NewClosureSniffTest.php
@@ -25,12 +25,33 @@ class NewClosureSniffTest extends BaseSniffTest
     /**
      * Test closures
      *
+     * @dataProvider dataClosure
+     *
+     * @param int $line The line number.
+     *
      * @return void
      */
-    public function testClosure()
+    public function testClosure($line)
     {
         $file = $this->sniffFile(self::TEST_FILE, '5.2');
-        $this->assertError($file, 3, 'Closures / anonymous functions are not available in PHP 5.2 or earlier');
+        $this->assertError($file, $line, 'Closures / anonymous functions are not available in PHP 5.2 or earlier');
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testClosure()
+     *
+     * @return array
+     */
+    public function dataClosure()
+    {
+        return array(
+            array(3),
+            array(14),
+            array(22),
+            array(30),
+        );
     }
 
 
@@ -47,13 +68,76 @@ class NewClosureSniffTest extends BaseSniffTest
 
 
     /**
+     * Test static closures
+     *
+     * @dataProvider dataStaticClosure
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testStaticClosure($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.3');
+        $this->assertError($file, $line, 'Closures / anonymous functions could not be declared as static in PHP 5.3 or earlier');
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testStaticClosure()
+     *
+     * @return array
+     */
+    public function dataStaticClosure()
+    {
+        return array(
+            array(14),
+            array(30),
+        );
+    }
+
+
+    /**
+     * Test using $this in closures
+     *
+     * @dataProvider dataThisInClosure
+     *
+     * @param int  $line            The line number.
+     * @param bool $testNoViolation Whether or not to run the noViolation test.
+     *
+     * @return void
+     */
+    public function testThisInClosure($line, $testNoViolation = true)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.3');
+        $this->assertError($file, $line, 'Closures / anonymous functions did not have access to $this in PHP 5.3 or earlier');
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testThisInClosure()
+     *
+     * @return array
+     */
+    public function dataThisInClosure()
+    {
+        return array(
+            array(23),
+            array(24),
+        );
+    }
+
+
+    /**
      * Verify no notices are thrown at all.
      *
      * @return void
      */
     public function testNoViolationsInFileOnValidVersion()
     {
-        $file = $this->sniffFile(self::TEST_FILE, '5.3');
+        $file = $this->sniffFile(self::TEST_FILE, '5.4');
         $this->assertNoViolation($file);
     }
 
--- Tests/sniff-examples/new_closure.php
+++ Tests/sniff-examples/new_closure.php
@@ -4,3 +4,29 @@ spl_autoload_register( function ( $class ) {
 } );
 
 function something() {}
+
+// PHP 5.4+,
+class Test
+{
+    // PHP 5.4+: Static closures.
+    public function testStatic()
+    {
+        return static function() {
+            echo 'static closure';
+        };
+    }
+
+    // PHP 5.4+: Using this in a class context.
+    public function testThis()
+    {
+        return function() {
+            var_dump($this);
+            var_dump($this); // Let's make sure we get an error for each line using $this.
+        };
+    }
+}
+
+// PHP 5.4+: Static closures.
+static function() {
+    var_dump($something);
+}
