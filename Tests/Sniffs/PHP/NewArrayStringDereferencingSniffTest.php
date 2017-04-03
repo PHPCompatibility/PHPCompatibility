e6874efd204fdac1178b125eaec605176b904115
jrfnl <github_nospam@adviesenzo.nl>
2017-04-03 06:15:43 +0200

    PHP 5.5: New sniff to detect array and string literal dereferencing
    > Array and string literals can now be dereferenced directly to access individual elements and characters
    Ref: http://php.net/manual/en/migration55.new-features.php#migration55.new-features.const-dereferencing

---
+++ Tests/Sniffs/PHP/NewArrayStringDereferencingSniffTest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * NewArrayStringDereferencingSniff test file.
+ *
+ * @package PHPCompatibility
+ */
+
+
+/**
+ * New array and string literal dereferencing sniff test.
+ *
+ * @group newArrayStringDereferencing
+ * @group dereferencing
+ *
+ * @covers PHPCompatibility_Sniffs_PHP_NewArrayStringDereferencingSniff
+ *
+ * @uses    BaseSniffTest
+ * @package PHPCompatibility
+ * @author  Juliette Reinders Folmer <phpcompatibility_nospam@adviesenzo.nl>
+ */
+class NewArrayStringDereferencingSniffTest extends BaseSniffTest
+{
+    const TEST_FILE = 'sniff-examples/new_array_string_dereferencing.php';
+
+    /**
+     * testArrayStringDereferencing
+     *
+     * @dataProvider dataArrayStringDereferencing
+     *
+     * @param int    $line The line number.
+     * @param string $type Whether this is an array or string dereferencing.
+     *
+     * @return void
+     */
+    public function testArrayStringDereferencing($line, $type)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.4');
+        $this->assertError($file, $line, "Direct array dereferencing of {$type} is not present in PHP version 5.4 or earlier");
+    }
+
+    /**
+     * Data provider dataArrayStringDereferencing.
+     *
+     * @see testArrayStringDereferencing()
+     *
+     * @return array
+     */
+    public function dataArrayStringDereferencing()
+    {
+        return array(
+            array(4, 'arrays'),
+            array(5, 'arrays'),
+            array(6, 'arrays'),
+            array(7, 'string literals'),
+            array(8, 'string literals'),
+        );
+    }
+
+
+    /**
+     * testNoFalsePositives
+     *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.4');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        return array(
+            array(11),
+            array(12),
+            array(13),
+            array(14),
+            array(15),
+            array(16),
+        );
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.5');
+        $this->assertNoViolation($file);
+    }
+
+}
