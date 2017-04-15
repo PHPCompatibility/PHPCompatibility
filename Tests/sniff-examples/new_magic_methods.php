73f1999e4c7aaee8843f9e8fc196b5e0978e7837
jrfnl <github_nospam@adviesenzo.nl>
2017-04-15 21:17:59 +0200

    Add unit tests for anonymous classes to the NewMagicMethodsSniff
    No new magic methods were introduced since the introduction of anonymous classes, but the sniff does allow for them and is now unit tested for it too.

--- Tests/sniff-examples/new_magic_methods.php
+++ Tests/sniff-examples/new_magic_methods.php
@@ -52,3 +52,18 @@ function __toString() {}
 function __callStatic($name, $arguments) {}
 function __invoke($x) {}
 function __debugInfo() {}
+
+/*
+ * Magic methods in anonymous classes.
+ */
+$a = new class
+{
+    public function __get($name) {}
+    public function __isset($name) {}
+    public function __unset($name) {}
+    public static function __set_state($properties) {}
+    public function __toString() {}
+    public static function __callStatic($name, $arguments) {}
+    public function __invoke($x) {}
+    public function __debugInfo() {}
+}
