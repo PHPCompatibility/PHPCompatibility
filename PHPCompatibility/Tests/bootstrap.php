b6c55057424eebe5edb7fe0c724cfdd46b394961
jrfnl <jrfnl@users.noreply.github.com>
2017-07-27 16:42:19 +0200

    Test bootstrap: Simplify the file loading logic

--- PHPCompatibility/Tests/bootstrap.php
+++ PHPCompatibility/Tests/bootstrap.php
@@ -18,43 +18,53 @@ if (defined('PHP_CODESNIFFER_VERBOSITY') === false) {
     define('PHP_CODESNIFFER_VERBOSITY', 0);
 }
 
+$ds = DIRECTORY_SEPARATOR;
+
 // Get the PHPCS dir from an environment variable.
 $phpcsDir = getenv('PHPCS_DIR');
 
-if ($phpcsDir === false) {
-    // Ok, no environment variable set, so this might be a PEAR install of PHPCS.
-    // @todo fix path for PEAR install!
-    $phpcsDir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..';
+// This may be a Composer install.
+if ($phpcsDir === false && is_dir(dirname(dirname(__DIR__)) . $ds . 'vendor' . $ds . 'squizlabs' . $ds . 'php_codesniffer')) {
+    $vendorDir = dirname(dirname(__DIR__)) . $ds . 'vendor';
+    $phpcsDir  = $vendorDir . $ds . 'squizlabs' . $ds . 'php_codesniffer';
+} elseif ($phpcsDir !== false) {
+    $phpcsDir = realpath($phpcsDir);
 }
-$phpcsDir .= DIRECTORY_SEPARATOR;
 
-if (file_exists($phpcsDir . 'autoload.php')) {
+// Try and load the PHPCS autoloader.
+if ($phpcsDir !== false && file_exists($phpcsDir . $ds . 'autoload.php')) {
     // PHPCS 3.x.
-    require $phpcsDir . 'autoload.php';
+    require_once $phpcsDir . $ds . 'autoload.php';
 
-} elseif (file_exists($phpcsDir . 'CodeSniffer.php')) {
+    /*
+     * Alias the PHPCS 3.x classes to their PHPCS 2.x equivalent if necessary.
+     * Also provide a custom autoloader for our abstract base classes as the PHPCS native autoloader
+     * has trouble with them in combination with the PHPCompatibility custom unit test suite.
+     */
+    include_once dirname(dirname(__DIR__)) . $ds . 'PHPCSAliases.php';
+
+} elseif ($phpcsDir !== false && file_exists($phpcsDir . $ds . 'CodeSniffer.php')) {
     // PHPCS 1.x, 2.x.
-    require $phpcsDir . 'CodeSniffer.php';
+    require_once $phpcsDir . $ds . 'CodeSniffer.php';
+
+    if (isset($vendorDir) && file_exists($vendorDir . $ds . 'autoload.php')) {
+        require_once $vendorDir . $ds . 'autoload.php';
+    }
 
 } else {
-    // Otherwise we must be in a composer install.
-    $vendorDir        = __DIR__ . '/../../vendor';
-    $composerAutoload = $vendorDir . DIRECTORY_SEPARATOR . 'autoload.php';
-    $phpcsAutoload    = $vendorDir . DIRECTORY_SEPARATOR . 'squizlabs' . DIRECTORY_SEPARATOR . 'php_codesniffer' . DIRECTORY_SEPARATOR . 'autoload.php';
+    echo 'Uh oh... can\'t find PHPCS.
 
-    if (!@include($composerAutoload)) {
-        echo 'You must set up the project dependencies, run the following commands:
-    wget http://getcomposer.org/composer.phar
-    php composer.phar install
-    ';
-        die(1);
-    }
+If you use Composer, please run `composer install --prefer-source`.
+Otherwise, make sure you set a `PHPCS_DIR` environment variable in your phpunit.xml file
+pointing to the PHPCS directory.
 
-    // PHPCS 3.x no longer uses Composer autoload, so include the PHPCS native autoloader.
-    if (file_exists($phpcsAutoload)) {
-        require_once $phpcsAutoload;
-    }
+Please read the contributors guidelines for more information:
+https://is.gd/PHPCompatibilityContrib
+';
+
+    die(1);
 }
+
 
 // PHPUnit cross version compatibility.
 if (class_exists('PHPUnit\Runner\Version')
@@ -64,17 +74,6 @@ if (class_exists('PHPUnit\Runner\Version')
     class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
 }
 
-
-require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'PHPCSHelper.php';
-
-/*
- * Alias the PHPCS 3.x classes to their PHPCS 2.x equivalent if necessary.
- * Also provide a custom autoloader for our abstract base classes as the PHPCS native autoloader
- * has trouble with them in combination with the PHPCompatibility custom unit test suite.
- */
-if (version_compare(\PHPCompatibility\PHPCSHelper::getVersion(), '2.99.99', '>')) {
-    include_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'PHPCSAliases.php';
-}
-
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'BaseSniffTest.php';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'BaseClass' . DIRECTORY_SEPARATOR . 'MethodTestFrame.php';
+require_once __DIR__ . $ds . 'BaseSniffTest.php';
+require_once __DIR__ . $ds . 'BaseClass' . $ds . 'MethodTestFrame.php';
+unset($ds, $phpcsDir, $vendorDir);
