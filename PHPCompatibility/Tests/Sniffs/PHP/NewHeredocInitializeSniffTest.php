4e5a3662f4be3b5844a43630c5fe9a16dc0d9fda
jrfnl <jrfnl@users.noreply.github.com>
2018-03-18 04:38:13 +0100

    NewHeredocInitialize: add unit tests for global constants, multi-declarations and function param defaults
    The PHP 5.3 change regarding initializing something with a heredoc turns out to apply to more than the current sniff was sniffing for.
    
    Add tests for:
    * Global constants declared using the `const` keyword
        A bit of a mood case as the `const` keyword was only introduced in PHP 5.3. All the same, the sniff should be accurate and the unit tests should reflect this.
    * Multi-declarations for static variables, constants and class properties
    * Default values for function parameters

--- PHPCompatibility/Tests/Sniffs/PHP/NewHeredocInitializeSniffTest.php
+++ PHPCompatibility/Tests/Sniffs/PHP/NewHeredocInitializeSniffTest.php
@@ -84,12 +84,23 @@ class NewHeredocInitializeSniffTest extends BaseSniffTest
     {
         return array(
             array(5, 'static variables'),
-            array(13, 'class constants'),
+            array(13, 'constants'),
             array(19, 'class properties'),
-            array(27, 'class constants'),
+            array(27, 'constants'),
             array(31, 'class properties'),
-            array(39, 'class constants'),
+            array(39, 'constants'),
             array(47, 'class properties', true),
+            array(52, 'constants'),
+            array(60, 'constants'),
+            array(87, 'static variables'),
+            array(90, 'static variables'),
+            array(97, 'constants'),
+            array(100, 'constants'),
+            array(104, 'class properties'),
+            array(107, 'class properties'),
+            array(115, 'default parameter values'),
+            array(121, 'default parameter values'),
+            array(124, 'default parameter values'),
         );
     }
 
@@ -119,8 +130,6 @@ class NewHeredocInitializeSniffTest extends BaseSniffTest
     public function dataNoFalsePositives()
     {
         return array(
-            array(57),
-            array(63),
             array(70),
             array(75),
             array(79),
