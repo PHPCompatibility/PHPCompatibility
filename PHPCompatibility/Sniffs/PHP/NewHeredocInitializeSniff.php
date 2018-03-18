dabd0caf18dae7bf93f80638442a16093a96e8ee
jrfnl <jrfnl@users.noreply.github.com>
2018-03-18 04:45:57 +0100

    NewHeredocInitialize: extend the NewScalarExpressions sniff
    The logic within the original `NewHeredocInitialize` sniff did not allow for multi-declarations nor for examining the default values of function parameters.
    
    While not (well) documented, these features are supported and do allow initialization with heredocs since PHP 5.3.
    
    As the logic to determine whether a token needs to be examined is the same for this sniff and for the new `NewScalarExpressions` sniff, letting the sniff extend the `NewScalarExpressions` sniff prevents code duplication.

--- PHPCompatibility/Sniffs/PHP/NewHeredocInitializeSniff.php
+++ PHPCompatibility/Sniffs/PHP/NewHeredocInitializeSniff.php
@@ -11,13 +11,18 @@
 
 namespace PHPCompatibility\Sniffs\PHP;
 
-use PHPCompatibility\Sniff;
+use PHPCompatibility\Sniffs\PHP\NewConstantScalarExpressionsSniff;
 
 /**
  * \PHPCompatibility\Sniffs\PHP\NewHeredocInitializeSniff.
  *
- * As of PHP 5.3.0, it's possible to initialize static variables and class
- * properties/constants using the Heredoc syntax.
+ * As of PHP 5.3.0, it's possible to initialize static variables, class properties
+ * and constants declared using the `const` keyword, using the Heredoc syntax.
+ * And while not documented, heredoc initialization can now also be used for function param defaults.
+ * See: https://3v4l.org/JVH8W
+ *
+ * These heredocs (still) cannot contain variables. That's, however, outside the scope of the
+ * PHPCompatibility library until such time as there is a PHP version in which this would be accepted.
  *
  * PHP version 5.3
  *
@@ -25,132 +30,59 @@ use PHPCompatibility\Sniff;
  * @package  PHPCompatibility
  * @author   Juliette Reinders Folmer <phpcompatibility_nospam@adviesenzo.nl>
  */
-class NewHeredocInitializeSniff extends Sniff
+class NewHeredocInitializeSniff extends NewConstantScalarExpressionsSniff
 {
 
     /**
-     * Returns an array of tokens this test wants to listen for.
+     * Error message.
      *
-     * @return array
+     * @var string
      */
-    public function register()
-    {
-        return array(T_START_HEREDOC);
-    }
+    const ERROR_PHRASE = 'Initializing %s using the Heredoc syntax was not supported in PHP 5.2 or earlier';
 
     /**
-     * Processes this test, when one of its tokens is encountered.
+     * Partial error phrases to be used in combination with the error message constant.
+     *
+     * @var array
+     */
+    protected $errorPhrases = array(
+        'const'     => 'constants',
+        'property'  => 'class properties',
+        'staticvar' => 'static variables',
+        'default'   => 'default parameter values',
+    );
+
+
+    /**
+     * Do a version check to determine if this sniff needs to run at all.
+     *
+     * @return bool
+     */
+    protected function bowOutEarly()
+    {
+        return ($this->supportsBelow('5.2') !== true);
+    }
+
+
+    /**
+     * Is a value declared and does the declared value not contain an heredoc ?
      *
      * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
      * @param int                   $stackPtr  The position of the current token in the
      *                                         stack passed in $tokens.
+     * @param int                   $end       The end of the value definition.
      *
-     * @return void
+     * @return bool True if no heredoc (or assignment) is found, false otherwise.
      */
-    public function process(\PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    protected function isValidAssignment(\PHP_CodeSniffer_File $phpcsFile, $stackPtr, $end)
     {
-        if ($this->supportsBelow('5.2') !== true) {
-            return;
-        }
-
         $tokens = $phpcsFile->getTokens();
-
-        $equalSign = $phpcsFile->findPrevious(\PHP_CodeSniffer_Tokens::$emptyTokens, ($stackPtr - 1), null, true, null, true);
-        if ($equalSign === false || $tokens[$equalSign]['code'] !== T_EQUAL) {
-            // Not an assignment.
-            return;
+        $next   = $phpcsFile->findNext(\PHP_CodeSniffer_Tokens::$emptyTokens, ($stackPtr + 1), $end, true);
+        if ($next === false || $tokens[$next]['code'] !== T_EQUAL) {
+            // No value assigned.
+            return true;
         }
 
-        $prevNonEmpty = $phpcsFile->findPrevious(\PHP_CodeSniffer_Tokens::$emptyTokens, ($equalSign - 1), null, true, null, true);
-        if ($prevNonEmpty === false
-            || ($tokens[$prevNonEmpty]['code'] !== T_VARIABLE
-                && $tokens[$prevNonEmpty]['code'] !== T_STRING)
-        ) {
-            // Not a variable or constant assignment.
-            return;
-        }
-
-        switch ($tokens[$prevNonEmpty]['type']) {
-            /*
-             * Check class constant assignments.
-             */
-            case 'T_STRING':
-                // Walk back to check for the const keyword.
-                $constPtr = $phpcsFile->findPrevious(\PHP_CodeSniffer_Tokens::$emptyTokens, ($prevNonEmpty - 1), null, true, null, true);
-                if ($constPtr === false || $tokens[$constPtr]['code'] !== T_CONST) {
-                    // Not a constant assignment.
-                    return;
-                }
-
-                if ($this->isClassConstant($phpcsFile, $constPtr) === true) {
-                    $this->throwError($phpcsFile, $stackPtr, 'const');
-                }
-                return;
-
-            case 'T_VARIABLE':
-                /*
-                 * Check class property assignments.
-                 */
-                if ($this->isClassProperty($phpcsFile, $prevNonEmpty) === true) {
-                    $this->throwError($phpcsFile, $stackPtr, 'property');
-                }
-
-                /*
-                 * Check static variable assignments.
-                 */
-                else {
-                    // Walk back to check this is a static variable `static $var =`.
-                    $staticPtr = $phpcsFile->findPrevious(\PHP_CodeSniffer_Tokens::$emptyTokens, ($prevNonEmpty - 1), null, true, null, true);
-                    if ($staticPtr === false || $tokens[$staticPtr]['code'] !== T_STATIC) {
-                        // Not a static variable assignment.
-                        return;
-                    }
-
-                    // Still here ? Then we have a static variable assignment.
-                    $this->throwError($phpcsFile, $stackPtr, 'staticvar');
-                }
-                return;
-        }
+        return ($phpcsFile->findNext(T_START_HEREDOC, ($next + 1), $end, false, null, true) === false);
     }
-
-
-    /**
-     * Throw an error if a non-static value is found.
-     *
-     * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
-     * @param int                   $stackPtr  The position of the token to link the error to.
-     * @param string                $type      Type of usage found.
-     *
-     * @return void
-     */
-    protected function throwError(\PHP_CodeSniffer_File $phpcsFile, $stackPtr, $type)
-    {
-        switch ($type) {
-            case 'const':
-                $phrase = 'class constants';
-                break;
-
-            case 'property':
-                $phrase = 'class properties';
-                break;
-
-            case 'staticvar':
-                $phrase = 'static variables';
-                break;
-
-            default:
-                $phrase = '';
-                break;
-        }
-
-        $errorCode = $this->stringToErrorCode($type) . 'Found';
-
-        $phpcsFile->addError(
-            'Initializing %s using the Heredoc syntax was not supported in PHP 5.2 or earlier',
-            $stackPtr,
-            $errorCode,
-            array($phrase)
-        );
-    }
-
 }
