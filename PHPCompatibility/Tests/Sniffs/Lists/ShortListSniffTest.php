f51557059a0771b3e2eee3d02094333dec786b8f
jrfnl <jrfnl@users.noreply.github.com>
2018-03-27 03:39:50 +0200

    :sparkles: PHP 7.1: New sniff to detect short list syntax for symmetric array destructuring
    > The shorthand array syntax ([]) may now be used to destructure arrays for assignments (including within foreach), as an alternative to the existing list() syntax, which is still supported.
    
    Refs:
    * http://php.net/manual/en/migration71.new-features.php#migration71.new-features.symmetric-array-destructuring
    * https://wiki.php.net/rfc/short_list_syntax
    * https://github.com/php/php-src/commit/4f077aee836ad7d8335cf62629a8364bdf939db9
    
    Fixes 248

---
+++ PHPCompatibility/Tests/Sniffs/Lists/ShortListSniffTest.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * PHP 7.1 symmetric array destructuring sniff test file.
+ *
+ * @package PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\Sniffs\Lists;
+
+use PHPCompatibility\Tests\BaseSniffTest;
+
+/**
+ * PHP 7.1 symmetric array destructuring sniff test file.
+ *
+ * @group shortList
+ * @group listAssignments
+ *
+ * @covers \PHPCompatibility\Sniffs\Lists\ShortListSniff
+ *
+ * @uses    \PHPCompatibility\Tests\BaseSniffTest
+ * @package PHPCompatibility
+ * @author  Juliette Reinders Folmer <phpcompatibility_nospam@adviesenzo.nl>
+ */
+class ShortListSniffTest extends BaseSniffTest
+{
+    const TEST_FILE = 'Sniffs/Lists/ShortListTestCases.inc';
+
+    /**
+     * testShortList
+     *
+     * @dataProvider dataShortList
+     *
+     * @param int $line Line number where the error should occur.
+     *
+     * @return void
+     */
+    public function testShortList($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '7.0');
+        $this->assertError($file, $line, 'The shorthand list syntax "[]" to destructure arrays is not available in PHP 7.0 or earlier.');
+    }
+
+    /**
+     * dataShortList
+     *
+     * @see testShortList()
+     *
+     * @return array
+     */
+    public function dataShortList()
+    {
+        return array(
+            array(17),
+            array(18),
+            array(19),
+            array(21),
+            array(23),
+            array(25), // x2.
+            array(28),
+        );
+    }
+
+
+    /**
+     * testNoFalsePositives
+     *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line Line number with a valid list assignment.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '7.0');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * dataNoFalsePositives
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        return array(
+            array(6),
+            array(8),
+            array(10),
+            array(12),
+            array(31),
+        );
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '7.1');
+        $this->assertNoViolation($file);
+    }
+
+}
