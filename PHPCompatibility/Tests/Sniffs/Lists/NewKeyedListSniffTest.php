269df94eb01d5f592c25e679d2761439817f7449
jrfnl <jrfnl@users.noreply.github.com>
2018-03-28 04:21:30 +0200

    :sparkles: PHP 7.1: New sniff to detect keys in list()
    > Support for keys in list()
    >
    > You can now specify keys in `list()`, or its new shorthand `[]` syntax. This enables destructuring of arrays with non-integer or non-sequential keys.
    
    Refs:
    * http://php.net/manual/en/migration71.new-features.php#migration71.new-features.support-for-keys-in-list
    * https://wiki.php.net/rfc/list_keys
    * https://github.com/php/php-src/commit/37c8bb58686b2d86f145ebe4fe39854f5951dcd7
    
    Notes:
    * An error will be thrown for each double arrow encountered. The error will be linked to the double arrow token to provide the closest indication of where the list key was found.
    * The sniff has been set up to allow for easily extending it to re-use the logic to a) determine the open/close tokens and b) to check within a list construct.
        I already have another sniff lined up which needs the same logic.
    * Includes extensive unit tests.
    
    Fixes 252

---
+++ PHPCompatibility/Tests/Sniffs/Lists/NewKeyedListSniffTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * PHP 7.1 keyed lists sniff test file.
+ *
+ * @package PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\Sniffs\Lists;
+
+use PHPCompatibility\Tests\BaseSniffTest;
+
+/**
+ * PHP 7.1 keyed lists sniff test file.
+ *
+ * @group newKeyedList
+ * @group listAssignments
+ *
+ * @covers \PHPCompatibility\Sniffs\Lists\NewKeyedListSniff
+ *
+ * @uses    \PHPCompatibility\Tests\BaseSniffTest
+ * @package PHPCompatibility
+ * @author  Juliette Reinders Folmer <phpcompatibility_nospam@adviesenzo.nl>
+ */
+class NewKeyedListSniffTest extends BaseSniffTest
+{
+    const TEST_FILE = 'Sniffs/Lists/NewKeyedListTestCases.inc';
+
+    /**
+     * testNewKeyedList
+     *
+     * @dataProvider dataNewKeyedList
+     *
+     * @param int $line Line number where the error should occur.
+     *
+     * @return void
+     */
+    public function testNewKeyedList($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '7.0');
+        $this->assertError($file, $line, 'Specifying keys in list constructs is not supported in PHP 7.0 or earlier.');
+    }
+
+    /**
+     * dataNewKeyedList
+     *
+     * @see testNewKeyedList()
+     *
+     * @return array
+     */
+    public function dataNewKeyedList()
+    {
+        return array(
+            array(15), // x3.
+            array(16), // x2.
+            array(17), // x2.
+            array(18),
+            array(19), // x2.
+            array(20), // x2.
+            array(22), // x3.
+            array(23), // x2.
+            array(28),
+            array(29),
+            array(30),
+            array(31),
+            array(36), // x2.
+            array(37), // x2.
+            array(41), // x2.
+            array(42), // x2.
+            array(46),
+            array(48),
+            array(58),
+            array(62),
+        );
+    }
+
+
+    /**
+     * testNoFalsePositives
+     *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line Line number with a valid list assignment.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '7.0');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * dataNoFalsePositives
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        return array(
+            array(6),
+            array(8),
+            array(10),
+            array(27),
+            array(35),
+            array(40),
+            array(45),
+            array(47),
+            array(49),
+        );
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '7.1');
+        $this->assertNoViolation($file);
+    }
+
+}
