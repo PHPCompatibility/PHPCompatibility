fa70198cf368f43e66b1045e58e7472bc842973b
jrfnl <jrfnl@users.noreply.github.com>
2018-04-02 03:48:46 +0200

    :sparkles: PHP 7.2: New sniff to detect passing `null` to get_class()
    > "Previously, passing NULL to the get_class() function would output the name of the enclosing class. This behaviour has now been removed, where an E_WARNING will be output instead.
    > To achieve the same behaviour as before, the argument should simply be omitted."
    
    Refs:
    * http://php.net/manual/en/migration72.incompatible.php#migration72.incompatible.no-null-to-get_class
    * https://wiki.php.net/rfc/get_class_disallow_null_parameter
    * http://php.net/manual/en/function.get-class.php
    
    Fixes 557

---
+++ PHPCompatibility/Tests/Sniffs/FunctionParameters/GetClassNullSniffTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Passing `null` to get_class() sniff test file.
+ *
+ * @package PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\Sniffs\FunctionParameters;
+
+use PHPCompatibility\Tests\BaseSniffTest;
+
+/**
+ * Passing `null` to get_class() sniff tests.
+ *
+ * @group getClassNull
+ * @group functionParameterValues
+ *
+ * @covers \PHPCompatibility\Sniffs\FunctionParameters\GetClassNullSniff
+ *
+ * @uses    \PHPCompatibility\Tests\BaseSniffTest
+ * @package PHPCompatibility
+ * @author  Juliette Reinders Folmer <phpcompatibility_nospam@adviesenzo.nl>
+ */
+class GetClassNullSniffTest extends BaseSniffTest
+{
+
+    const TEST_FILE = 'Sniffs/FunctionParameters/GetClassNullTestCases.inc';
+
+    /**
+     * testGetClassNull
+     *
+     * @dataProvider dataGetClassNull
+     *
+     * @param int $line Line number where the error should occur.
+     *
+     * @return void
+     */
+    public function testGetClassNull($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '7.2');
+        $this->assertError($file, $line, 'Passing "null" as the $object to get_class() is not allowed since PHP 7.2.');
+    }
+
+    /**
+     * dataGetClassNull
+     *
+     * @see testGetClassNull()
+     *
+     * @return array
+     */
+    public function dataGetClassNull()
+    {
+        return array(
+            array(11),
+        );
+    }
+
+
+    /**
+     * testNoFalsePositives
+     *
+     * @return void
+     */
+    public function testNoFalsePositives()
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '7.2');
+
+        // No errors expected on the first 9 lines.
+        for ($line = 1; $line <= 9; $line++) {
+            $this->assertNoViolation($file, $line);
+        }
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '7.1');
+        $this->assertNoViolation($file);
+    }
+}
