beeefa277c51d4d1fac6825326fbceecbae9ac83
jrfnl <jrfnl@users.noreply.github.com>
2018-04-02 05:54:30 +0200

    :sparkles: PHP 5.6: New sniff to detect passing deprecated $type values to iconv_get_encoding
    "The iconv and mbstring configuration options related to encoding have been deprecated in favour of default_charset."
    
    Refs:
    * http://php.net/manual/en/migration56.deprecated.php#migration56.deprecated.iconv-mbstring-encoding
    * https://wiki.php.net/rfc/default_encoding
    
    Note:
    As it is unclear which MBString functions to target, the sniff only checks the affected `iconv` function.
    Based on the commit and the other documentation, I get the impression that for MBString, this only affects the ini directives.
    
    Fixes 475

---
+++ PHPCompatibility/Tests/Sniffs/FunctionParameters/IconvEncodingSniffTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Deprecated Iconv encoding types sniff test file.
+ *
+ * @package PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\Sniffs\FunctionParameters;
+
+use PHPCompatibility\Tests\BaseSniffTest;
+
+/**
+ * Deprecated Iconv encoding types sniff tests.
+ *
+ * @group iconvEncoding
+ * @group functionParameterValues
+ *
+ * @covers \PHPCompatibility\Sniffs\FunctionParameters\IconvEncodingSniff
+ *
+ * @uses    \PHPCompatibility\Tests\BaseSniffTest
+ * @package PHPCompatibility
+ * @author  Juliette Reinders Folmer <phpcompatibility_nospam@adviesenzo.nl>
+ */
+class IconvEncodingSniffTest extends BaseSniffTest
+{
+
+    const TEST_FILE = 'Sniffs/FunctionParameters/IconvEncodingTestCases.inc';
+
+    /**
+     * testIconvEncoding
+     *
+     * @dataProvider dataIconvEncoding
+     *
+     * @param int $line Line number where the error should occur.
+     *
+     * @return void
+     */
+    public function testIconvEncoding($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.6');
+        $this->assertWarning($file, $line, 'All previously accepted values for the $type parameter of iconv_set_encoding() have been deprecated since PHP 5.6.');
+    }
+
+    /**
+     * dataIconvEncoding
+     *
+     * @see testIconvEncoding()
+     *
+     * @return array
+     */
+    public function dataIconvEncoding()
+    {
+        return array(
+            array(14),
+            array(15),
+            array(16),
+            array(17),
+            array(18),
+        );
+    }
+
+
+    /**
+     * testNoFalsePositives
+     *
+     * @return void
+     */
+    public function testNoFalsePositives()
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.6');
+
+        // No errors expected on the first 10 lines.
+        for ($line = 1; $line <= 10; $line++) {
+            $this->assertNoViolation($file, $line);
+        }
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.5');
+        $this->assertNoViolation($file);
+    }
+}
