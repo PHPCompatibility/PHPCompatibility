5d05cc3d3e7d2e8aaf16a573335fa13c0e2e69f7
jrfnl <jrfnl@users.noreply.github.com>
2018-04-04 04:40:34 +0200

    :sparkles: PHP 5.5: New sniff to detect referenced expressions prior to PHP 5.5
    > 5.5.0 	Referencing of $value is supported for expressions. Formerly, only variables have been supported.
    
    > Before PHP 5.5.0, referencing $value is only possible if the iterated array can be referenced (i.e. if it is a variable).
    
    Ref: http://php.net/manual/en/control-structures.foreach.php
    
    Note: this change is not mentioned anywhere in the PHP 5.5 changelog, but tests show it has been implemented and that the documentation is correct.
    See: https://3v4l.org/gFvaZ

---
+++ PHPCompatibility/Tests/Sniffs/ControlStructures/NewForeachExpressionReferencingUnitTest.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * PHP 5.5 referencing expressions in foreach sniff test file.
+ *
+ * @package PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\Sniffs\ControlStructures;
+
+use PHPCompatibility\Tests\BaseSniffTest;
+
+/**
+ * PHP 5.5 referencing expressions in foreach sniff test file.
+ *
+ * @group newForeachExpressionReferencing
+ * @group controlStructures
+ *
+ * @covers \PHPCompatibility\Sniffs\ControlStructures\NewForeachExpressionReferencingSniff
+ *
+ * @uses    \PHPCompatibility\Tests\BaseSniffTest
+ * @package PHPCompatibility
+ * @author  Juliette Reinders Folmer <phpcompatibility_nospam@adviesenzo.nl>
+ */
+class NewForeachExpressionReferencingUnitTest extends BaseSniffTest
+{
+    const TEST_FILE = 'Sniffs/ControlStructures/NewForeachExpressionReferencingUnitTest.inc';
+
+    /**
+     * testNewForeachExpressionReferencing
+     *
+     * @dataProvider dataNewForeachExpressionReferencing
+     *
+     * @param int $line Line number where the error should occur.
+     *
+     * @return void
+     */
+    public function testNewForeachExpressionReferencing($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.4');
+        $this->assertError($file, $line, 'Referencing $value is only possible if the iterated array is a variable in PHP 5.4 or earlier.');
+    }
+
+    /**
+     * dataNewForeachExpressionReferencing
+     *
+     * @see testNewForeachExpressionReferencing()
+     *
+     * @return array
+     */
+    public function dataNewForeachExpressionReferencing()
+    {
+        return array(
+            array(17),
+            array(18),
+            array(20),
+            array(21),
+            array(23),
+            array(24),
+        );
+    }
+
+
+    /**
+     * testNoFalsePositives
+     *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line Line number with a valid list assignment.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives($line)
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.4');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * dataNoFalsePositives
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        return array(
+            array(6),
+            array(7),
+            array(8),
+            array(9),
+            array(10),
+            array(11),
+            array(12),
+        );
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(self::TEST_FILE, '5.5');
+        $this->assertNoViolation($file);
+    }
+}
