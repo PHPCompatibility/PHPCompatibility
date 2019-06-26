e518ecf0f830bf60c9519798221e32fde09248ee
jrfnl <jrfnl@users.noreply.github.com>
2019-06-26 16:08:14 +0200

    :sparkles: PHP 5.3: new ForbiddenToStringParameters sniff (part 2)
    > The __toString() magic method can no longer accept arguments.
    
    Ref: https://www.php.net/manual/en/migration53.incompatible.php
    
    This introduces a sister-sniff called `PHPCompatibility.MethodUse.ForbiddenToStringParameters` to the `PHPCompatibility.FunctionDeclarations.ForbiddenToStringParameters` which will detect parameters being passed in direct calls to the magic `__toString()` method. (as per my previous remark in 815)

--- PHPCompatibility/Sniffs/FunctionDeclarations/ForbiddenToStringParametersSniff.php
+++ PHPCompatibility/Sniffs/FunctionDeclarations/ForbiddenToStringParametersSniff.php
@@ -17,6 +17,8 @@ use PHP_CodeSniffer_File as File;
 /**
  * As of PHP 5.3, the __toString() magic method can no longer accept arguments.
  *
+ * Sister-sniff to PHPCompatibility.MethodUse.ForbiddenToStringParameters.
+ *
  * @link https://www.php.net/manual/en/migration53.incompatible.php
  *
  * PHP version 5.3
