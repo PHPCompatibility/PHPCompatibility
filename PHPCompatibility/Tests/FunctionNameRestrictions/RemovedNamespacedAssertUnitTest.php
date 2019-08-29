d6852f970d886772dddac02ebfa4f829ec23a568
jrfnl <jrfnl@users.noreply.github.com>
2019-08-29 18:55:19 +0200

    RemovedNamespacedAssert: remove work-arounds for PHP 5.3/ PHPCS < 2.6.0 (tests only)

--- PHPCompatibility/Tests/FunctionNameRestrictions/RemovedNamespacedAssertUnitTest.php
+++ PHPCompatibility/Tests/FunctionNameRestrictions/RemovedNamespacedAssertUnitTest.php
@@ -11,7 +11,6 @@
 namespace PHPCompatibility\Tests\FunctionNameRestrictions;
 
 use PHPCompatibility\Tests\BaseSniffTest;
-use PHPCompatibility\PHPCSHelper;
 
 /**
  * Test the RemovedNamespacedAssert sniff.
@@ -40,28 +39,6 @@ class RemovedNamespacedAssertUnitTest extends BaseSniffTest
      * @var string
      */
     const TEST_FILE_NAMESPACED = 'RemovedNamespacedAssertUnitTest.2.inc';
-
-    /**
-     * Whether or not traits and interfaces will be properly scoped in PHPCS.
-     *
-     * @var bool
-     */
-    protected static $recognizesTraitsAndInterfaces = true;
-
-    /**
-     * Set up skip condition.
-     *
-     * @return void
-     */
-    public static function setUpBeforeClass()
-    {
-        // When using PHPCS 2.3.4 or lower combined with PHP 5.3 or lower, traits are
-        // not recognized and the scope condition for interfaces will not be set.
-        if (version_compare(PHPCSHelper::getVersion(), '2.4.0', '<') && version_compare(\PHP_VERSION_ID, '50400', '<')) {
-            self::$recognizesTraitsAndInterfaces = false;
-        }
-        parent::setUpBeforeClass();
-    }
 
     /**
      * Test deprecation of namespaced free-standing assert() function declaration.
@@ -99,19 +76,13 @@ class RemovedNamespacedAssertUnitTest extends BaseSniffTest
      *
      * @dataProvider dataNoFalsePositives
      *
-     * @param string $testFile  The file to test.
-     * @param int    $line      The line number.
-     * @param bool   $maybeSkip Whether this test needs to be skipped on old PHP/PHPCS combis.
+     * @param string $testFile The file to test.
+     * @param int    $line     The line number.
      *
      * @return void
      */
-    public function testNoFalsePositives($testFile, $line, $maybeSkip = false)
+    public function testNoFalsePositives($testFile, $line)
     {
-        if ($maybeSkip === true && self::$recognizesTraitsAndInterfaces === false) {
-            $this->markTestSkipped('Traits are not recognized and interface scope conditions not added on PHPCS < 2.4.0 in combination with PHP < 5.4');
-            return;
-        }
-
         $file = $this->sniffFile(__DIR__ . '/' . $testFile, '7.3');
         $this->assertNoViolation($file, $line);
     }
@@ -132,8 +103,8 @@ class RemovedNamespacedAssertUnitTest extends BaseSniffTest
             array(self::TEST_FILE, 14),
             array(self::TEST_FILE, 18),
             array(self::TEST_FILE_NAMESPACED, 8),
-            array(self::TEST_FILE_NAMESPACED, 12, true),
-            array(self::TEST_FILE_NAMESPACED, 16, true),
+            array(self::TEST_FILE_NAMESPACED, 12),
+            array(self::TEST_FILE_NAMESPACED, 16),
             array(self::TEST_FILE_NAMESPACED, 20),
         );
     }
