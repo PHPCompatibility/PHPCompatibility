7e137ab3d655327b323712d66bdbc52296fb8ec3
jrfnl <jrfnl@users.noreply.github.com>
2019-12-16 05:15:35 +0100

    :sparkles: PHP 7.0: New NewNestedStaticAccess sniff
    This sniff only covers a small part of the changes in the PHP 7.0 Uniform Variable Syntax RFC, but it's at least another part which is covered.
    
    > ```php
    > // support nested ::
    > $foo['bar']::$baz
    > $foo::$bar::$baz
    > $foo->bar()::baz()
    > ```
    >
    > Static property fetches and method calls can now be applied to any dereferencable expression. E.g. `$foo['bar']::$baz`, `$foo::$bar::$baz` and `$foo->bar()::baz()` are all valid now.
    
    Ref: https://wiki.php.net/rfc/uniform_variable_syntax
    
    The sniff implementation as is being pulled now is supported by various tests run on 3v4l:
    https://3v4l.org/up9e3
    https://3v4l.org/7tZcL
    https://3v4l.org/5uuFR
    https://3v4l.org/grUNF
    https://3v4l.org/mtWn9
    https://3v4l.org/PAtnX
    https://3v4l.org/qujtp
    https://3v4l.org/Rrteo
    https://3v4l.org/GQA2P
    https://3v4l.org/2XCUZ
    https://3v4l.org/d3vcp
    
    Includes unit tests.
    
    The sniff has been run over a number of large PHP projects to see if any false positives could be found, but didn't find any.
    
    Fixes 946

---
+++ PHPCompatibility/Tests/Syntax/NewNestedStaticAccessUnitTest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\Syntax;
+
+use PHPCompatibility\Tests\BaseSniffTest;
+
+/**
+ * Test the NewNestedStaticAccess sniff.
+ *
+ * @group newNestedStaticAccess
+ * @group syntax
+ *
+ * @covers \PHPCompatibility\Sniffs\Syntax\NewNestedStaticAccessSniff
+ *
+ * @since 10.0.0
+ */
+class NewNestedStaticAccessUnitTest extends BaseSniffTest
+{
+
+    /**
+     * testNestedStaticAccess
+     *
+     * @dataProvider dataNestedStaticAccess
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNestedStaticAccess($line)
+    {
+        $file = $this->sniffFile(__FILE__, '5.6');
+        $this->assertError($file, $line, 'Nested access to static properties, constants and methods was not supported in PHP 5.6 or earlier.');
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNestedStaticAccess()
+     *
+     * @return array
+     */
+    public function dataNestedStaticAccess()
+    {
+        return array(
+            array(7),
+            array(8),
+            array(9),
+            array(10),
+            array(11),
+            array(12),
+            array(13),
+            array(14),
+            array(15),
+            array(16),
+            array(17),
+            array(18),
+            array(19),
+            array(23),
+        );
+    }
+
+
+    /**
+     * Verify the sniff doesn't throw false positives.
+     *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives($line)
+    {
+        $file = $this->sniffFile(__FILE__, '5.6');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        return array(
+            array(4),
+            array(27),
+            array(28),
+            array(31),
+            array(34),
+            array(35),
+        );
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '7.0');
+        $this->assertNoViolation($file);
+    }
+}
