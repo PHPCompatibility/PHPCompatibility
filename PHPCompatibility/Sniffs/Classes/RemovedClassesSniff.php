da4ccc997fa8a157d877e50473c0d6660ae4e85d
jrfnl <jrfnl@users.noreply.github.com>
2020-05-10 09:43:11 +0200

    :sparkles: New `PHPCompatibility.Classes.RemovedClasses` sniff
    Initial implementation includes removed classes from the PHP 5.3 removed Ming extension.
    
    Note: This sniff currently mirrors the logic in the `NewClasses` sniff.
    Use of PHPCSUtils and the upcoming `RemovedExtensions` trait will be implemented separately.

---
+++ PHPCompatibility/Sniffs/Classes/RemovedClassesSniff.php
@@ -0,0 +1,429 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Sniffs\Classes;
+
+use PHPCompatibility\AbstractRemovedFeatureSniff;
+use PHP_CodeSniffer_File as File;
+
+/**
+ * Detect use of removed PHP native classes.
+ *
+ * The sniff analyses the following constructs to find usage of removed classes:
+ * - Class instantiation using the `new` keyword.
+ * - (Anonymous) Class declarations to detect removed classes being extended by userland classes.
+ * - Static use of class properties, constants or functions using the double colon.
+ * - Function/closure declarations to detect removed classes used as parameter type declarations.
+ * - Function/closure declarations to detect removed classes used as return type declarations.
+ * - Try/catch statements to detect removed exception classes being caught.
+ *
+ * PHP version All
+ *
+ * @since 10.0.0
+ */
+class RemovedClassesSniff extends AbstractRemovedFeatureSniff
+{
+
+    /**
+     * A list of deprecated/removed classes, not present in older versions.
+     *
+     * The array lists : version number with false (deprecated) and true (removed).
+     * If's sufficient to list the first version where the class was deprecated/removed.
+     *
+     * The optional `extension` key should be used to list the name of the extension
+     * the class comes from if this class is part of a removed extension and should
+     * match the array in the RemovedExtensionsTrait.
+     *
+     * @since 10.0.0
+     *
+     * @var array(string => array(string => bool))
+     */
+    protected $removedClasses = array(
+
+        'SWFAction' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFBitmap' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFButton' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFDisplayItem' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFFill' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFFont' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFFontChar' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFGradient' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFMorph' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFMovie' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFPrebuiltClip' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFShape' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFSound' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFSoundInstance' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFSprite' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFText' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFTextField' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+        'SWFVideoStream' => array(
+            '5.3'       => true,
+            'extension' => 'ming',
+        ),
+    );
+
+    /**
+     * A list of deprecated/removed Exception classes.
+     *
+     * The array lists : version number with false (deprecated) and true (removed).
+     * If's sufficient to list the first version where the class was deprecated/removed.
+     *
+     * The optional `extension` key should be used to list the name of the extension
+     * the class comes from if this class is part of a removed extension and should
+     * match the array in the ReovedExtensionsTrait.
+     *
+     * {@internal Classes listed here do not need to be added to the $removedClasses
+     *            property as well.
+     *            This list is automatically added to the $removedClasses property
+     *            in the `register()` method.}
+     *
+     * @since 10.0.0
+     *
+     * @var array(string => array(string => bool))
+     */
+    protected $removedExceptions = array();
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @since 10.0.0
+     *
+     * @return array
+     */
+    public function register()
+    {
+        // Handle case-insensitivity of class names.
+        $this->removedClasses    = \array_change_key_case($this->removedClasses, \CASE_LOWER);
+        $this->removedExceptions = \array_change_key_case($this->removedExceptions, \CASE_LOWER);
+
+        // Add the Exception classes to the Classes list.
+        $this->removedClasses = array_merge($this->removedClasses, $this->removedExceptions);
+
+        return array(
+            \T_NEW,
+            \T_CLASS,
+            \T_ANON_CLASS,
+            \T_DOUBLE_COLON,
+            \T_FUNCTION,
+            \T_CLOSURE,
+            \T_CATCH,
+            \T_RETURN_TYPE,
+        );
+    }
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @since 10.0.0
+     *
+     * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                   $stackPtr  The position of the current token in
+     *                                         the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        switch ($tokens[$stackPtr]['type']) {
+            case 'T_FUNCTION':
+            case 'T_CLOSURE':
+                $this->processFunctionToken($phpcsFile, $stackPtr);
+
+                // Deal with older PHPCS version which don't recognize return type hints
+                // as well as newer PHPCS versions (3.3.0+) where the tokenization has changed.
+                $returnTypeHint = $this->getReturnTypeHintToken($phpcsFile, $stackPtr);
+                if ($returnTypeHint !== false) {
+                    $this->processReturnTypeToken($phpcsFile, $returnTypeHint);
+                }
+                break;
+
+            case 'T_CATCH':
+                $this->processCatchToken($phpcsFile, $stackPtr);
+                break;
+
+            case 'T_RETURN_TYPE':
+                $this->processReturnTypeToken($phpcsFile, $stackPtr);
+                break;
+
+            default:
+                $this->processSingularToken($phpcsFile, $stackPtr);
+                break;
+        }
+    }
+
+
+    /**
+     * Processes this test for when a token resulting in a singular class name is encountered.
+     *
+     * @since 10.0.0
+     *
+     * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                   $stackPtr  The position of the current token in
+     *                                         the stack passed in $tokens.
+     *
+     * @return void
+     */
+    private function processSingularToken(File $phpcsFile, $stackPtr)
+    {
+        $tokens      = $phpcsFile->getTokens();
+        $FQClassName = '';
+
+        if ($tokens[$stackPtr]['type'] === 'T_NEW') {
+            $FQClassName = $this->getFQClassNameFromNewToken($phpcsFile, $stackPtr);
+
+        } elseif ($tokens[$stackPtr]['type'] === 'T_CLASS' || $tokens[$stackPtr]['type'] === 'T_ANON_CLASS') {
+            $FQClassName = $this->getFQExtendedClassName($phpcsFile, $stackPtr);
+
+        } elseif ($tokens[$stackPtr]['type'] === 'T_DOUBLE_COLON') {
+            $FQClassName = $this->getFQClassNameFromDoubleColonToken($phpcsFile, $stackPtr);
+        }
+
+        if ($FQClassName === '') {
+            return;
+        }
+
+        $className   = substr($FQClassName, 1); // Remove global namespace indicator.
+        $classNameLc = strtolower($className);
+
+        if (isset($this->removedClasses[$classNameLc]) === false) {
+            return;
+        }
+
+        $itemInfo = array(
+            'name'   => $className,
+            'nameLc' => $classNameLc,
+        );
+        $this->handleFeature($phpcsFile, $stackPtr, $itemInfo);
+    }
+
+
+    /**
+     * Processes this test for when a function token is encountered.
+     *
+     * - Detect removed classes when used as a parameter type declaration.
+     *
+     * @since 10.0.0
+     *
+     * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                   $stackPtr  The position of the current token in
+     *                                         the stack passed in $tokens.
+     *
+     * @return void
+     */
+    private function processFunctionToken(File $phpcsFile, $stackPtr)
+    {
+        // Retrieve typehints stripped of global NS indicator and/or nullable indicator.
+        $typeHints = $this->getTypeHintsFromFunctionDeclaration($phpcsFile, $stackPtr);
+        if (empty($typeHints) || \is_array($typeHints) === false) {
+            return;
+        }
+
+        foreach ($typeHints as $hint) {
+
+            $typeHintLc = strtolower($hint);
+
+            if (isset($this->removedClasses[$typeHintLc]) === true) {
+                $itemInfo = array(
+                    'name'   => $hint,
+                    'nameLc' => $typeHintLc,
+                );
+                $this->handleFeature($phpcsFile, $stackPtr, $itemInfo);
+            }
+        }
+    }
+
+
+    /**
+     * Processes this test for when a catch token is encountered.
+     *
+     * - Detect exceptions when used in a catch statement.
+     *
+     * @since 10.0.0
+     *
+     * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                   $stackPtr  The position of the current token in
+     *                                         the stack passed in $tokens.
+     *
+     * @return void
+     */
+    private function processCatchToken(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        // Bow out during live coding.
+        if (isset($tokens[$stackPtr]['parenthesis_opener'], $tokens[$stackPtr]['parenthesis_closer']) === false) {
+            return;
+        }
+
+        $opener = $tokens[$stackPtr]['parenthesis_opener'];
+        $closer = ($tokens[$stackPtr]['parenthesis_closer'] + 1);
+        $name   = '';
+        $listen = array(
+            // Parts of a (namespaced) class name.
+            \T_STRING              => true,
+            \T_NS_SEPARATOR        => true,
+            // End/split tokens.
+            \T_VARIABLE            => false,
+            \T_BITWISE_OR          => false,
+            \T_CLOSE_CURLY_BRACKET => false, // Shouldn't be needed as we expect a var before this.
+        );
+
+        for ($i = ($opener + 1); $i < $closer; $i++) {
+            if (isset($listen[$tokens[$i]['code']]) === false) {
+                continue;
+            }
+
+            if ($listen[$tokens[$i]['code']] === true) {
+                $name .= $tokens[$i]['content'];
+                continue;
+            } else {
+                if (empty($name) === true) {
+                    // Weird, we should have a name by the time we encounter a variable or |.
+                    // So this may be the closer.
+                    continue;
+                }
+
+                $name   = ltrim($name, '\\');
+                $nameLC = strtolower($name);
+
+                if (isset($this->removedExceptions[$nameLC]) === true) {
+                    $itemInfo = array(
+                        'name'   => $name,
+                        'nameLc' => $nameLC,
+                    );
+                    $this->handleFeature($phpcsFile, $i, $itemInfo);
+                }
+
+                // Reset for a potential multi-catch.
+                $name = '';
+            }
+        }
+    }
+
+
+    /**
+     * Processes this test for when a return type token is encountered.
+     *
+     * - Detect removed classes when used as a return type declaration.
+     *
+     * @since 10.0.0
+     *
+     * @param \PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                   $stackPtr  The position of the current token in
+     *                                         the stack passed in $tokens.
+     *
+     * @return void
+     */
+    private function processReturnTypeToken(File $phpcsFile, $stackPtr)
+    {
+        $returnTypeHint = $this->getReturnTypeHintName($phpcsFile, $stackPtr);
+        if (empty($returnTypeHint)) {
+            return;
+        }
+
+        $returnTypeHint   = ltrim($returnTypeHint, '\\');
+        $returnTypeHintLc = strtolower($returnTypeHint);
+
+        if (isset($this->removedClasses[$returnTypeHintLc]) === false) {
+            return;
+        }
+
+        // Still here ? Then this is a return type declaration using a new class.
+        $itemInfo = array(
+            'name'   => $returnTypeHint,
+            'nameLc' => $returnTypeHintLc,
+        );
+        $this->handleFeature($phpcsFile, $stackPtr, $itemInfo);
+    }
+
+
+    /**
+     * Get the relevant sub-array for a specific item from a multi-dimensional array.
+     *
+     * @since 10.0.0
+     *
+     * @param array $itemInfo Base information about the item.
+     *
+     * @return array Version and other information about the item.
+     */
+    public function getItemArray(array $itemInfo)
+    {
+        return $this->removedClasses[$itemInfo['nameLc']];
+    }
+
+
+    /**
+     * Get the error message template for this sniff.
+     *
+     * @since 10.0.0
+     *
+     * @return string
+     */
+    protected function getErrorMsgTemplate()
+    {
+        return 'The built-in class %s is ';
+    }
+}
---
+++ PHPCompatibility/Tests/Classes/RemovedClassesUnitTest.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\Classes;
+
+use PHPCompatibility\Tests\BaseSniffTest;
+
+/**
+ * Test the RemovedClasses sniff.
+ *
+ * @group removedClasses
+ * @group classes
+ *
+ * @covers \PHPCompatibility\Sniffs\Classes\RemovedClassesSniff
+ *
+ * @since 10.0.0
+ */
+class RemovedClassesUnitTest extends BaseSniffTest
+{
+
+    /**
+     * testRemovedClass
+     *
+     * @dataProvider dataRemovedClass
+     *
+     * @param string $className      Class name.
+     * @param string $removedIn      The PHP version in which the class was removed.
+     * @param array  $lines          The line numbers in the test file which apply to this class.
+     * @param string $okVersion      A PHP version in which the class was still valid.
+     * @param string $removedVersion Optional PHP version to test removed message with -
+     *                               if different from the $removedIn version.
+     *
+     * @return void
+     */
+    public function testRemovedClass($className, $removedIn, $lines, $okVersion, $removedVersion = null)
+    {
+        $file = $this->sniffFile(__FILE__, $okVersion);
+        foreach ($lines as $line) {
+            $this->assertNoViolation($file, $line);
+        }
+
+        $errorVersion = (isset($removedVersion)) ? $removedVersion : $removedIn;
+        $file         = $this->sniffFile(__FILE__, $errorVersion);
+        $error        = "The built-in class {$className} is removed since PHP {$removedIn}";
+        foreach ($lines as $line) {
+            $this->assertError($file, $line, $error);
+        }
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testRemovedClass()
+     *
+     * @return array
+     */
+    public function dataRemovedClass()
+    {
+        return array(
+            array('SWFAction', '5.3', array(32, 33, 34, 35), '5.2'),
+            array('SWFBitmap', '5.3', array(37), '5.2'),
+            array('SWFButton', '5.3', array(38), '5.2'),
+            array('SWFDisplayItem', '5.3', array(39), '5.2'),
+            array('SWFFill', '5.3', array(40), '5.2'),
+            array('SWFFont', '5.3', array(41), '5.2'),
+            array('SWFFontChar', '5.3', array(44), '5.2'),
+            array('SWFGradient', '5.3', array(45), '5.2'),
+            array('SWFMorph', '5.3', array(46), '5.2'),
+            array('SWFMovie', '5.3', array(47), '5.2'),
+            array('SWFPrebuiltClip', '5.3', array(48), '5.2'),
+            array('SWFShape', '5.3', array(49), '5.2'),
+            array('SWFSound', '5.3', array(50), '5.2'),
+            array('SWFSoundInstance', '5.3', array(51), '5.2'),
+            array('SWFSprite', '5.3', array(52), '5.2'),
+            array('SWFText', '5.3', array(55), '5.2'),
+            array('SWFTextField', '5.3', array(56), '5.2'),
+            array('SWFVideoStream', '5.3', array(57), '5.2'),
+        );
+    }
+
+
+    /**
+     * Verify the sniff doesn't flag classes which aren't deprecated/removed.
+     *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives($line)
+    {
+        $file = $this->sniffFile(__FILE__, '99.0'); // High version beyond latest deprecation.
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        // No errors expected on the first 26 lines.
+        $data = array();
+        for ($line = 1; $line <= 26; $line++) {
+            $data[] = array($line);
+        }
+
+        return $data;
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '5.0'); // Low version below the first deprecation.
+        $this->assertNoViolation($file);
+    }
+}
