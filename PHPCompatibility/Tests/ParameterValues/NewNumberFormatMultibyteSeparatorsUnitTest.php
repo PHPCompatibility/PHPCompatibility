6e225cce985c398a1a078e25ad543fa9041de45a
jrfnl <jrfnl@users.noreply.github.com>
2020-05-12 18:08:54 +0200

    :sparkles: PHP 5.4: New `PHPCompatibility.ParameterValues.NewNumberFormatMultibyteSeparators` sniff
    From: https://www.php.net/manual/en/function.number-format.php#refsect1-function.number-format-changelog
    
    > This function now supports multiple bytes in `dec_point` and `thousands_sep`. Only the first byte of each separator was used in older versions.
    
    This sniff addresses that change.
    
    Includes unit tests.

---
+++ PHPCompatibility/Tests/ParameterValues/NewNumberFormatMultibyteSeparatorsUnitTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\ParameterValues;
+
+use PHPCompatibility\Tests\BaseSniffTest;
+
+/**
+ * Test the NewNumberFormatMultibyteSeparators sniff.
+ *
+ * @group newNumberFormatMultibyteSeparators
+ * @group parameterValues
+ *
+ * @covers \PHPCompatibility\Sniffs\ParameterValues\NewNumberFormatMultibyteSeparatorsSniff
+ *
+ * @since 10.0.0
+ */
+class NewNumberFormatMultibyteSeparatorsUnitTest extends BaseSniffTest
+{
+
+    /**
+     * testNewNumberFormatMultibyteSeparators
+     *
+     * @dataProvider dataNewNumberFormatMultibyteSeparators
+     *
+     * @param int    $line      Line number where the error should occur.
+     * @param string $paramName The name of the parameter for which to expect the error.
+     *
+     * @return void
+     */
+    public function testNewNumberFormatMultibyteSeparators($line, $paramName)
+    {
+        $file = $this->sniffFile(__FILE__, '5.3');
+        $this->assertError($file, $line, "Passing a multi-byte separator as the \${$paramName} to number_format() is not supported in PHP 5.3 or earlier");
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNewNumberFormatMultibyteSeparators()
+     *
+     * @return array
+     */
+    public function dataNewNumberFormatMultibyteSeparators()
+    {
+        return array(
+            array(20, 'thousands_sep'),
+            array(24, 'dec_point'),
+            array(29, 'dec_point'),
+            array(33, 'thousands_sep'),
+        );
+    }
+
+
+    /**
+     * Verify there are no false positives on valid code.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives()
+    {
+        $file = $this->sniffFile(__FILE__, '5.3');
+
+        // No errors expected on the first 16 lines.
+        for ($line = 1; $line <= 16; $line++) {
+            $this->assertNoViolation($file, $line);
+        }
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '5.4');
+        $this->assertNoViolation($file);
+    }
+}
