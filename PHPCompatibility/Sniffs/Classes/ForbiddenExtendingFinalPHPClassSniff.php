e25f3d5efa2bb63cd9369963559ffe0712061dff
jrfnl <jrfnl@users.noreply.github.com>
2020-07-12 08:13:20 +0200

    :sparkles: New `PHPCompatibility.Classes.ForbiddenExtendingFinalClass` sniff
    > Implemented FR #78638 (__PHP_Incomplete_Class should be final)
    
    Refs:
    * https://github.com/php/php-src/blob/0e45ed772df304c58f151d75d75f4ab5d9192c5b/NEWS#L1526
    * https://github.com/php/php-src/commit/b418f335d4a764c5d1d4ae59d6c1d0a0bd2550f4
    
    To address this change, I'm introducing a new array based sniff which allows for more classes to be added if/when more native PHP classes would become `final`.
    
    Includes unit tests.
    Includes XML docs.

---
+++ PHPCompatibility/Sniffs/Classes/ForbiddenExtendingFinalPHPClassSniff.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Sniffs\Classes;
+
+use PHPCompatibility\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+/**
+ * Detect extending of PHP native classes which became final at some point.
+ *
+ * PHP version 8.0+
+ *
+ * @since 10.0.0
+ */
+class ForbiddenExtendingFinalPHPClassSniff extends Sniff
+{
+
+    /**
+     * A list of PHP native classes which became final at some point.
+     *
+     * @since 10.0.0
+     *
+     * @var array(string => int) Key is the fully qualified classname.
+     *                           Value the PHP version in which the class became final.
+     */
+    protected $finalClasses = [
+        '\__PHP_Incomplete_Class' => '8.0',
+    ];
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @since 10.0.0
+     *
+     * @return array
+     */
+    public function register()
+    {
+        // Handle case-insensitivity of class names.
+        $this->finalClasses = \array_change_key_case($this->finalClasses, \CASE_LOWER);
+
+        return [
+            \T_CLASS,
+            \T_ANON_CLASS,
+        ];
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @since 10.0.0
+     *
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+     * @param int                         $stackPtr  The position of the current token in
+     *                                               the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $FQClassName = $this->getFQExtendedClassName($phpcsFile, $stackPtr);
+        if ($FQClassName === '') {
+            return;
+        }
+
+        $classNameLc = \strtolower($FQClassName);
+
+        if (isset($this->finalClasses[$classNameLc]) === false) {
+            return;
+        }
+
+        if ($this->supportsAbove($this->finalClasses[$classNameLc]) === false) {
+            return;
+        }
+
+        $data = [
+            \substr($FQClassName, 1), // Remove global namespace indicator.
+            $this->finalClasses[$classNameLc],
+        ];
+
+        $phpcsFile->addError(
+            'The built-in class %s is final since PHP %s and cannot be extended',
+            $stackPtr,
+            'Found',
+            $data
+        );
+    }
+}
