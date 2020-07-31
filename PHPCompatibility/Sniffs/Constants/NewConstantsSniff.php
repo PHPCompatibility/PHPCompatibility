04b351744614ad95070f2784314fae3d4d83d715
jrfnl <jrfnl@users.noreply.github.com>
2020-08-01 01:08:11 +0200

    PHP 8.0: NewConstants: add new `T_NULLSAFE_OBJECT_OPERATOR` constant
    Ref: https://wiki.php.net/rfc/nullsafe_operator
    
    ...and exclude it from triggering when running PHPCompatibility over its own code.

--- PHPCompatibility/Sniffs/Constants/NewConstantsSniff.php
+++ PHPCompatibility/Sniffs/Constants/NewConstantsSniff.php
@@ -6470,6 +6470,10 @@ class NewConstantsSniff extends AbstractNewFeatureSniff
             '7.4' => false,
             '8.0' => true,
         ),
+        'T_NULLSAFE_OBJECT_OPERATOR' => array(
+            '7.4' => false,
+            '8.0' => true,
+        ),
     );
 
 
