eaca566463832e942f20fd77f9e1b4232ff5616a
jrfnl <jrfnl@users.noreply.github.com>
2021-04-02 12:55:18 +0200

    NewFunctions: implement the new ComplexVersionNewFeatureTrait

--- PHPCompatibility/Sniffs/FunctionUse/NewFunctionsSniff.php
+++ PHPCompatibility/Sniffs/FunctionUse/NewFunctionsSniff.php
@@ -10,7 +10,8 @@
 
 namespace PHPCompatibility\Sniffs\FunctionUse;
 
-use PHPCompatibility\AbstractNewFeatureSniff;
+use PHPCompatibility\Sniff;
+use PHPCompatibility\Helpers\ComplexVersionNewFeatureTrait;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Util\Tokens;
 
@@ -20,12 +21,14 @@ use PHP_CodeSniffer\Util\Tokens;
  * PHP version All
  *
  * @since 5.5
- * @since 5.6   Now extends the base `Sniff` class instead of the upstream
- *              `Generic.PHP.ForbiddenFunctions` sniff.
- * @since 7.1.0 Now extends the `AbstractNewFeatureSniff` instead of the base `Sniff` class..
+ * @since 5.6    Now extends the base `Sniff` class instead of the upstream
+ *               `Generic.PHP.ForbiddenFunctions` sniff.
+ * @since 7.1.0  Now extends the `AbstractNewFeatureSniff` instead of the base `Sniff` class..
+ * @since 10.0.0 Now extends the base `Sniff` class and uses the `ComplexVersionNewFeatureTrait`.
  */
-class NewFunctionsSniff extends AbstractNewFeatureSniff
+class NewFunctionsSniff extends Sniff
 {
+    use ComplexVersionNewFeatureTrait;
 
     /**
      * A list of new functions, not present in older versions.
@@ -4721,29 +4724,54 @@ class NewFunctionsSniff extends AbstractNewFeatureSniff
 
 
     /**
-     * Get the relevant sub-array for a specific item from a multi-dimensional array.
+     * Handle the retrieval of relevant information and - if necessary - throwing of an
+     * error for a matched item.
      *
-     * @since 7.1.0
+     * @since 10.0.0
      *
-     * @param array $itemInfo Base information about the item.
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+     * @param int                         $stackPtr  The position of the relevant token in
+     *                                               the stack.
+     * @param array                       $itemInfo  Base information about the item.
      *
-     * @return array Version and other information about the item.
+     * @return void
      */
-    public function getItemArray(array $itemInfo)
+    protected function handleFeature(File $phpcsFile, $stackPtr, array $itemInfo)
     {
-        return $this->newFunctions[$itemInfo['nameLc']];
+        $itemArray   = $this->newFunctions[$itemInfo['nameLc']];
+        $versionInfo = $this->getVersionInfo($itemArray);
+
+        if (empty($versionInfo['not_in_version'])
+            || $this->supportsBelow($versionInfo['not_in_version']) === false
+        ) {
+            return;
+        }
+
+        $this->addError($phpcsFile, $stackPtr, $itemInfo, $versionInfo);
     }
 
 
     /**
-     * Get the error message template for this sniff.
+     * Generates the error for this item.
      *
-     * @since 7.1.0
+     * @since 10.0.0
      *
-     * @return string
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile   The file being scanned.
+     * @param int                         $stackPtr    The position of the relevant token in
+     *                                                 the stack.
+     * @param array                       $itemInfo    Base information about the item.
+     * @param string[]                    $versionInfo Array with detail (version) information
+     *                                                 relevant to the item.
+     *
+     * @return void
      */
-    protected function getErrorMsgTemplate()
+    protected function addError(File $phpcsFile, $stackPtr, array $itemInfo, array $versionInfo)
     {
-        return 'The function %s() is not present in PHP version %s or earlier';
+        // Overrule the default message template.
+        $this->msgTemplate = 'The function %s() is not present in PHP version %s or earlier';
+
+        $msgInfo = $this->getMessageInfo($itemInfo['name'], $itemInfo['name'], $versionInfo);
+
+        $phpcsFile->addError($msgInfo['message'], $stackPtr, $msgInfo['errorcode'], $msgInfo['data']);
     }
 }
