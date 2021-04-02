6bdfdae332dd62ae13ecea9a2f71e25becb5d335
jrfnl <jrfnl@users.noreply.github.com>
2021-04-02 04:11:04 +0200

    NewConstants: implement the new ComplexVersionNewFeatureTrait

--- PHPCompatibility/Sniffs/Constants/NewConstantsSniff.php
+++ PHPCompatibility/Sniffs/Constants/NewConstantsSniff.php
@@ -10,7 +10,8 @@
 
 namespace PHPCompatibility\Sniffs\Constants;
 
-use PHPCompatibility\AbstractNewFeatureSniff;
+use PHPCompatibility\Sniff;
+use PHPCompatibility\Helpers\ComplexVersionNewFeatureTrait;
 use PHP_CodeSniffer\Files\File;
 
 /**
@@ -19,9 +20,11 @@ use PHP_CodeSniffer\Files\File;
  * PHP version All
  *
  * @since 8.1.0
+ * @since 10.0.0 Now extends the base `Sniff` class and uses the `ComplexVersionNewFeatureTrait`.
  */
-class NewConstantsSniff extends AbstractNewFeatureSniff
+class NewConstantsSniff extends Sniff
 {
+    use ComplexVersionNewFeatureTrait;
 
     /**
      * A list of new PHP Constants, not present in older versions.
@@ -6672,29 +6675,54 @@ class NewConstantsSniff extends AbstractNewFeatureSniff
 
 
     /**
-     * Get the relevant sub-array for a specific item from a multi-dimensional array.
+     * Handle the retrieval of relevant information and - if necessary - throwing of an
+     * error for a matched item.
      *
-     * @since 8.1.0
+     * @since 10.0.0
      *
-     * @param array $itemInfo Base information about the item.
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+     * @param int                         $stackPtr  The position of the relevant token in
+     *                                               the stack.
+     * @param array                       $itemInfo  Base information about the item.
      *
-     * @return array Version and other information about the item.
+     * @return void
      */
-    public function getItemArray(array $itemInfo)
+    protected function handleFeature(File $phpcsFile, $stackPtr, array $itemInfo)
     {
-        return $this->newConstants[$itemInfo['name']];
+        $itemArray   = $this->newConstants[$itemInfo['name']];
+        $versionInfo = $this->getVersionInfo($itemArray);
+
+        if (empty($versionInfo['not_in_version'])
+            || $this->supportsBelow($versionInfo['not_in_version']) === false
+        ) {
+            return;
+        }
+
+        $this->addError($phpcsFile, $stackPtr, $itemInfo, $versionInfo);
     }
 
 
     /**
-     * Get the error message template for this sniff.
+     * Generates the error for this item.
      *
-     * @since 8.1.0
+     * @since 10.0.0
      *
-     * @return string
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile   The file being scanned.
+     * @param int                         $stackPtr    The position of the relevant token in
+     *                                                 the stack.
+     * @param array                       $itemInfo    Base information about the item.
+     * @param string[]                    $versionInfo Array with detail (version) information
+     *                                                 relevant to the item.
+     *
+     * @return void
      */
-    protected function getErrorMsgTemplate()
+    protected function addError(File $phpcsFile, $stackPtr, array $itemInfo, array $versionInfo)
     {
-        return 'The constant "%s" is not present in PHP version %s or earlier';
+        // Overrule the default message template.
+        $this->msgTemplate = 'The constant "%s" is not present in PHP version %s or earlier';
+
+        $msgInfo = $this->getMessageInfo($itemInfo['name'], $itemInfo['name'], $versionInfo);
+
+        $phpcsFile->addError($msgInfo['message'], $stackPtr, $msgInfo['errorcode'], $msgInfo['data']);
     }
 }
