PHPCompatibility/PHPCompatibility

commit 610e79ccb07f185cc47d9e1324029e88299c08c9

PHP 8.0: OptionalToRequiredFunctionParameters - handle required params for (gm)mktime()
> mktime() and gmmktime() now require at least one argument. `time()` can be
> used to get the current timestamp.

Refs:
* https://github.com/php/php-src/blob/69888c3ff1f2301ead8e37b23ff8481d475e29d2/UPGRADING#L192-L194
* https://github.com/php/php-src/commit/5a2787a02d56003e2e9731e2fad5b468180ec19a

Includes adjusted unit tests.

PHPCompatibility/Sniffs/FunctionUse/OptionalToRequiredFunctionParametersSniff.php
@@ -49,6 +49,19 @@ class OptionalToRequiredFunctionParametersSniff extends RequiredToOptionalFuncti
                 '5.6'  => 'recommended',
             ),
         ),
+        'gmmktime' => array(
+            1 => array(
+                'name' => 'hour',
+                '8.0'  => true,
+            ),
+        ),
+        'mktime' => array(
+            0 => array(
+                'name' => 'hour',
+                '5.1'  => false,
+                '8.0'  => true,
+            ),
+        ),
         'parse_str' => array(
             1 => array(
                 'name' => 'result',
@@ -165,7 +178,7 @@ class OptionalToRequiredFunctionParametersSniff extends RequiredToOptionalFuncti
             }
 
             // Remove the last 'and' from the message.
-            $error = substr($error, 0, (\strlen($error) - 5));
+            $error = \substr($error, 0, (\strlen($error) - 5));
         }
 
         $this->addMessage($phpcsFile, $error, $stackPtr, $errorInfo['error'], $errorCode, $data);

PHPCompatibility/Tests/FunctionUse/OptionalToRequiredFunctionParametersUnitTest.inc
@@ -9,3 +9,11 @@ crypt( $str ); // Recommended.
 
 // Prevent false positive on new. Issue #913.
 $crypt = new Crypt('password');
+
+// OK.
+gmmktime($hour, $minute, $second);
+mktime($hour);
+
+// Not OK.
+gmmktime();
+mktime();

PHPCompatibility/Tests/FunctionUse/OptionalToRequiredFunctionParametersUnitTest.php
@@ -68,6 +68,97 @@ class OptionalToRequiredFunctionParametersUnitTest extends BaseSniffTest
 
 
     /**
+     * testOptionalRequiredParameterDeprecatedRemoved
+     *
+     * @dataProvider dataOptionalRequiredParameterDeprecatedRemoved
+     *
+     * @param string $functionName     Function name.
+     * @param string $parameterName    Parameter name.
+     * @param string $softRequiredFrom The last PHP version in which the parameter was still optional (deprecated).
+     * @param string $hardRequiredFrom The last PHP version in which the parameter was still optional (removed).
+     * @param array  $lines            The line numbers in the test file which apply to this class.
+     * @param string $okVersion        A PHP version in which to test for no violation.
+     *
+     * @return void
+     */
+    public function testOptionalRequiredParameterDeprecatedRemoved($functionName, $parameterName, $softRequiredFrom, $hardRequiredFrom, $lines, $okVersion)
+    {
+        $file  = $this->sniffFile(__FILE__, $softRequiredFrom);
+        $error = "The \"{$parameterName}\" parameter for function {$functionName}() is missing. Passing this parameter is no longer optional. The optional nature of the parameter is deprecated since PHP {$softRequiredFrom}";
+        foreach ($lines as $line) {
+            $this->assertWarning($file, $line, $error);
+        }
+
+        $file  = $this->sniffFile(__FILE__, $hardRequiredFrom);
+        $error = "The \"{$parameterName}\" parameter for function {$functionName}() is missing. Passing this parameter is no longer optional. The optional nature of the parameter is deprecated since PHP {$softRequiredFrom} and removed since PHP {$hardRequiredFrom}";
+        foreach ($lines as $line) {
+            $this->assertError($file, $line, $error);
+        }
+
+        $file = $this->sniffFile(__FILE__, $okVersion);
+        foreach ($lines as $line) {
+            $this->assertNoViolation($file, $line);
+        }
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testOptionalRequiredParameterDeprecatedRemoved()
+     *
+     * @return array
+     */
+    public function dataOptionalRequiredParameterDeprecatedRemoved()
+    {
+        return array(
+            array('mktime', 'hour', '5.1', '8.0', array(19), '5.0'),
+        );
+    }
+
+
+    /**
+     * testOptionalRequiredParameterRemoved
+     *
+     * @dataProvider dataOptionalRequiredParameterRemoved
+     *
+     * @param string $functionName     Function name.
+     * @param string $parameterName    Parameter name.
+     * @param string $hardRequiredFrom The last PHP version in which the parameter was still optional.
+     * @param array  $lines            The line numbers in the test file which apply to this class.
+     * @param string $okVersion        A PHP version in which to test for no violation.
+     *
+     * @return void
+     */
+    public function testOptionalRequiredParameterRemoved($functionName, $parameterName, $hardRequiredFrom, $lines, $okVersion)
+    {
+        $file  = $this->sniffFile(__FILE__, $hardRequiredFrom);
+        $error = "The \"{$parameterName}\" parameter for function {$functionName}() is missing. Passing this parameter is no longer optional. The optional nature of the parameter is removed since PHP {$hardRequiredFrom}";
+        foreach ($lines as $line) {
+            $this->assertError($file, $line, $error);
+        }
+
+        $file = $this->sniffFile(__FILE__, $okVersion);
+        foreach ($lines as $line) {
+            $this->assertNoViolation($file, $line);
+        }
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testOptionalRequiredParameterRemoved()
+     *
+     * @return array
+     */
+    public function dataOptionalRequiredParameterRemoved()
+    {
+        return array(
+            array('gmmktime', 'hour', '8.0', array(18), '7.4'),
+        );
+    }
+
+
+    /**
      * testOptionalRecommendedParameter
      *
      * @dataProvider dataOptionalRecommendedParameter
@@ -135,6 +226,8 @@ class OptionalToRequiredFunctionParametersUnitTest extends BaseSniffTest
     {
         return array(
             array(4),
+            array(14),
+            array(15),
         );
     }
 
@@ -146,7 +239,7 @@ class OptionalToRequiredFunctionParametersUnitTest extends BaseSniffTest
      */
     public function testNoViolationsInFileOnValidVersion()
     {
-        $file = $this->sniffFile(__FILE__, '5.5'); // Version before earliest required/optional change.
+        $file = $this->sniffFile(__FILE__, '5.0'); // Version before earliest required/optional change.
         $this->assertNoViolation($file);
     }
 }

commit ca1724c7d0cf5b577f9f9d9f24025dc86eeee0b7

OptionalToRequiredFunctionParameters: bug fix for `gmmktime()`
From https://github.com/php/php-src/blob/69888c3ff1f2301ead8e37b23ff8481d475e29d2/UPGRADING#L192-L194 :
> `mktime()` and `gmmktime()` now require at least one argument. time() can be
used to get the current timestamp.

This commit fixes a bug for the offset being wrong for the `gmmktime()` function (introduced in
https://github.com/PHPCompatibility/PHPCompatibility/commit/610e79ccb07f185cc47d9e1324029e88299c08c9 ).

Note: as the change for `gmmktime()` and `mktime()` is actually "require at least one argument", if we combine that with named parameters, we may need to change how we sniff this as currently, as we'd report when the `hour` param is missing, even when one of the other parameters would be passed.

PHPCompatibility/Sniffs/FunctionUse/OptionalToRequiredFunctionParametersSniff.php
@@ -57,7 +57,7 @@ class OptionalToRequiredFunctionParametersSniff extends AbstractFunctionCallPara
             ],
         ],
         'gmmktime' => [
-            2 => [
+            1 => [
                 'name' => 'hour',
                 '8.0'  => true,
             ],