1dc7854718abe9c963569f28abdfd3a361d930ee
jrfnl <jrfnl@users.noreply.github.com>
2022-12-27 07:27:38 +0100

    :sparkles: PHP 8.1: New `PHPCompatibility.InitialValue.NewNewInDefine` sniff
    > `new` in Initializers
    >
    > [...] Objects can also be passed to `define()` now.
    
    Refs:
    * https://www.php.net/manual/en/migration81.new-features.php#migration81.new-features.core.new-in-initializer
    * https://github.com/php/php-src/pull/7149
    * https://github.com/php/php-src/commit/53aed48e5de2493787738f2945653110c30d694f
    
    Includes unit tests.

---
+++ PHPCompatibility/Tests/InitialValue/NewNewInDefineUnitTest.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\InitialValue;
+
+use PHPCompatibility\Tests\BaseSniffTest;
+
+/**
+ * Test the NewNewInDefine sniff.
+ *
+ * @group newNewInDefine
+ * @group initialValue
+ *
+ * @covers \PHPCompatibility\Sniffs\InitialValue\NewNewInDefineSniff
+ *
+ * @since 10.0.0
+ */
+final class NewNewInDefineUnitTest extends BaseSniffTest
+{
+
+    /**
+     * Verify that objects (new class instantiations) being passed to define() are detected correctly.
+     *
+     * @dataProvider dataNewInDefine
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNewInDefine($line)
+    {
+        $file = $this->sniffFile(__FILE__, '8.0');
+        $this->assertError($file, $line, 'Passing an object as the value when declaring constants using define is not allowed in PHP 8.0 or earlier');
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNewInDefine()
+     *
+     * @return array
+     */
+    public function dataNewInDefine()
+    {
+        return [
+            [38],
+            [39],
+            [40],
+            [41],
+            [44],
+            [45],
+            [46],
+            [51],
+        ];
+    }
+
+
+    /**
+     * Verify the sniff doesn't throw false positives for valid code.
+     *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNoFalsePositives($line)
+    {
+        $file = $this->sniffFile(__FILE__, '8.0');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        $data = [];
+        for ($line = 1; $line <= 34; $line++) {
+            $data[] = [$line];
+        }
+
+        return $data;
+    }
+
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '8.1');
+        $this->assertNoViolation($file);
+    }
+}
