0ae512351b62ebef36964b2b8253eddf155a7cdc
jrfnl <jrfnl@users.noreply.github.com>
2023-04-04 00:04:28 +0200

    ParameterValues/ForbiddenSessionModuleNameUser: add support for PHP 7.3+ indented heredoc/nowdocs
    Includes unit tests.
    
    Note: tests for PHP 7.3+ indented heredoc/nowdocs are in a separate file as the tokenizer cannot handle them on PHP < 7.3.

--- PHPCompatibility/Tests/ParameterValues/ForbiddenSessionModuleNameUserUnitTest.php
+++ PHPCompatibility/Tests/ParameterValues/ForbiddenSessionModuleNameUserUnitTest.php
@@ -26,7 +26,21 @@ class ForbiddenSessionModuleNameUserUnitTest extends BaseSniffTest
 {
 
     /**
-     * testForbiddenSessionModuleNameUser
+     * The name of the primary test case file.
+     *
+     * @var string
+     */
+    const TEST_FILE = 'ForbiddenSessionModuleNameUserUnitTest.1.inc';
+
+    /**
+     * The name of a secondary test case file containing PHP 7.3+ indented heredocs.
+     *
+     * @var string
+     */
+    const TEST_FILE_PHP73HEREDOCS = 'ForbiddenSessionModuleNameUserUnitTest.2.inc';
+
+    /**
+     * Verify detection of the issue.
      *
      * @dataProvider dataForbiddenSessionModuleNameUser
      *
@@ -36,7 +50,7 @@ class ForbiddenSessionModuleNameUserUnitTest extends BaseSniffTest
      */
     public function testForbiddenSessionModuleNameUser($line)
     {
-        $file = $this->sniffFile(__FILE__, '7.2');
+        $file = $this->sniffFile(__DIR__ . '/' . self::TEST_FILE, '7.2');
         $this->assertError($file, $line, 'Passing "user" as the $module to session_module_name() is not allowed since PHP 7.2.');
     }
 
@@ -60,27 +74,141 @@ class ForbiddenSessionModuleNameUserUnitTest extends BaseSniffTest
     /**
      * Verify there are no false positives on valid code.
      *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line The line number.
+     *
      * @return void
      */
-    public function testNoFalsePositives()
+    public function testNoFalsePositives($line)
     {
-        $file = $this->sniffFile(__FILE__, '7.2');
+        $file = $this->sniffFile(__DIR__ . '/' . self::TEST_FILE, '7.2');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        $cases = [];
 
         // No errors expected on the first 14 lines.
         for ($line = 1; $line <= 14; $line++) {
-            $this->assertNoViolation($file, $line);
+            $cases[] = [$line];
         }
+
+        return $cases;
+    }
+
+
+    /**
+     * Verify correct detection in combination with indented heredoc/nowdocs.
+     *
+     * @dataProvider dataForbiddenSessionModuleNameUserInIndentedHeredoc
+     *
+     * @param int $line Line number where the error should occur.
+     *
+     * @return void
+     */
+    public function testForbiddenSessionModuleNameUserInIndentedHeredoc($line)
+    {
+        if (\PHP_VERSION_ID < 70300) {
+            $this->markTestSkipped('Test code involving PHP 7.3 heredocs will not tokenize correctly on PHP < 7.3');
+        }
+
+        $file = $this->sniffFile(__DIR__ . '/' . self::TEST_FILE_PHP73HEREDOCS, '7.2');
+        $this->assertError($file, $line, 'Passing "user" as the $module to session_module_name() is not allowed since PHP 7.2.');
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testForbiddenSessionModuleNameUserInIndentedHeredoc()
+     *
+     * @return array
+     */
+    public function dataForbiddenSessionModuleNameUserInIndentedHeredoc()
+    {
+        return [
+            [16],
+        ];
+    }
+
+
+    /**
+     * Verify there are no false positives on valid code with indented heredoc/nowdocs.
+     *
+     * @dataProvider dataNoFalsePositivesInIndentedHeredoc
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNoFalsePositivesInIndentedHeredoc($line)
+    {
+        if (\PHP_VERSION_ID < 70300) {
+            $this->markTestSkipped('Test code involving PHP 7.3 heredocs will not tokenize correctly on PHP < 7.3');
+        }
+
+        $file = $this->sniffFile(__DIR__ . '/' . self::TEST_FILE_PHP73HEREDOCS, '7.2');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositivesInIndentedHeredoc()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositivesInIndentedHeredoc()
+    {
+        $cases = [];
+
+        // No errors expected on the first 14 lines.
+        for ($line = 1; $line <= 14; $line++) {
+            $cases[] = [$line];
+        }
+
+        return $cases;
     }
 
 
     /**
      * Verify no notices are thrown at all.
      *
+     * @dataProvider dataTestFiles
+     *
+     * @param string $testFile File name for the test case file to use.
+     *
      * @return void
      */
-    public function testNoViolationsInFileOnValidVersion()
+    public function testNoViolationsInFileOnValidVersion($testFile)
     {
-        $file = $this->sniffFile(__FILE__, '7.1');
+        if ($testFile === self::TEST_FILE_PHP73HEREDOCS && \PHP_VERSION_ID < 70300) {
+            $this->markTestSkipped('Test code involving PHP 7.3 heredocs will not tokenize correctly on PHP < 7.3');
+        }
+
+        $file = $this->sniffFile(__DIR__ . '/' . $testFile, '7.1');
         $this->assertNoViolation($file);
+    }
+
+
+    /**
+     * Data provider.
+     *
+     * @return array
+     */
+    public function dataTestFiles()
+    {
+        return [
+            [self::TEST_FILE],
+            [self::TEST_FILE_PHP73HEREDOCS],
+        ];
     }
 }
