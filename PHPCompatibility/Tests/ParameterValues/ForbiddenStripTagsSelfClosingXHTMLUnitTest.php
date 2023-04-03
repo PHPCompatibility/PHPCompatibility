48a242066b0105ee8d2f2c1591c7e22912106b2a
jrfnl <jrfnl@users.noreply.github.com>
2023-04-04 00:59:37 +0200

    ParameterValues/ForbiddenStripTagsSelfClosingXHTML: add tests with heredocs/nowdocs
    Includes safeguarding support for PHP 7.3+ indented heredoc/nowdocs.
    
    Note: tests for PHP 7.3+ indented heredoc/nowdocs are in a separate file as the tokenizer cannot handle them on PHP < 7.3.

--- PHPCompatibility/Tests/ParameterValues/ForbiddenStripTagsSelfClosingXHTMLUnitTest.php
+++ PHPCompatibility/Tests/ParameterValues/ForbiddenStripTagsSelfClosingXHTMLUnitTest.php
@@ -26,7 +26,21 @@ class ForbiddenStripTagsSelfClosingXHTMLUnitTest extends BaseSniffTest
 {
 
     /**
-     * testForbiddenStripTagsSelfClosingXHTML
+     * The name of the primary test case file.
+     *
+     * @var string
+     */
+    const TEST_FILE = 'ForbiddenStripTagsSelfClosingXHTMLUnitTest.1.inc';
+
+    /**
+     * The name of a secondary test case file containing PHP 7.3+ indented heredocs.
+     *
+     * @var string
+     */
+    const TEST_FILE_PHP73HEREDOCS = 'ForbiddenStripTagsSelfClosingXHTMLUnitTest.2.inc';
+
+    /**
+     * Verify detection of the issue.
      *
      * @dataProvider dataForbiddenStripTagsSelfClosingXHTML
      *
@@ -37,7 +51,7 @@ class ForbiddenStripTagsSelfClosingXHTMLUnitTest extends BaseSniffTest
      */
     public function testForbiddenStripTagsSelfClosingXHTML($line, $paramValue)
     {
-        $file  = $this->sniffFile(__FILE__, '5.4');
+        $file  = $this->sniffFile(__DIR__ . '/' . self::TEST_FILE, '5.4');
         $error = 'Self-closing XHTML tags are ignored. Only non-self-closing tags should be used in the strip_tags() $allowed_tags parameter since PHP 5.3.4. Found: ' . $paramValue;
 
         $this->assertError($file, $line, $error);
@@ -55,6 +69,7 @@ class ForbiddenStripTagsSelfClosingXHTMLUnitTest extends BaseSniffTest
         return [
             [14, "'<br/>'"],
             [15, "'<img/><br/>' . '<meta/><input/>'"],
+            [27, "<<<EOD\n<img/><br/>" . '$extraTags' . "\nEOD"],
         ];
     }
 
@@ -62,27 +77,148 @@ class ForbiddenStripTagsSelfClosingXHTMLUnitTest extends BaseSniffTest
     /**
      * Test the sniff does not throw false positives.
      *
+     * @dataProvider dataNoFalsePositives
+     *
+     * @param int $line The line number.
+     *
      * @return void
      */
-    public function testNoFalsePositives()
+    public function testNoFalsePositives($line)
     {
-        $file = $this->sniffFile(__FILE__, '5.4');
+        $file = $this->sniffFile(__DIR__ . '/' . self::TEST_FILE, '5.4');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositives()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositives()
+    {
+        $cases = [];
 
         // No errors expected on the first 12 lines.
         for ($line = 1; $line <= 12; $line++) {
-            $this->assertNoViolation($file, $line);
+            $cases[] = [$line];
         }
+
+        // No errors expected for the _correct_ heredoc/nowdocs.
+        for ($line = 17; $line <= 23; $line++) {
+            $cases[] = [$line];
+        }
+
+        return $cases;
+    }
+
+
+    /**
+     * Verify correct detection in combination with indented heredoc/nowdocs.
+     *
+     * @dataProvider dataForbiddenStripTagsSelfClosingXHTMLInIndentedHeredoc
+     *
+     * @param int  $line       Line number where the error should occur.
+     * @param bool $paramValue The parameter value detected.
+     *
+     * @return void
+     */
+    public function testForbiddenStripTagsSelfClosingXHTMLInIndentedHeredoc($line, $paramValue)
+    {
+        if (\PHP_VERSION_ID < 70300) {
+            $this->markTestSkipped('Test code involving PHP 7.3 heredocs will not tokenize correctly on PHP < 7.3');
+        }
+
+        $file  = $this->sniffFile(__DIR__ . '/' . self::TEST_FILE_PHP73HEREDOCS, '5.4');
+        $error = 'Self-closing XHTML tags are ignored. Only non-self-closing tags should be used in the strip_tags() $allowed_tags parameter since PHP 5.3.4. Found: ' . $paramValue;
+
+        $this->assertError($file, $line, $error);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testForbiddenStripTagsSelfClosingXHTMLInIndentedHeredoc()
+     *
+     * @return array
+     */
+    public function dataForbiddenStripTagsSelfClosingXHTMLInIndentedHeredoc()
+    {
+        return [
+            [18, "<<<'EOD'\n    <meta/><input/>\n    EOD"],
+        ];
+    }
+
+
+    /**
+     * Test the sniff does not throw false positives.
+     *
+     * @dataProvider dataNoFalsePositivesInIndentedHeredoc
+     *
+     * @param int $line The line number.
+     *
+     * @return void
+     */
+    public function testNoFalsePositivesInIndentedHeredoc($line)
+    {
+        if (\PHP_VERSION_ID < 70300) {
+            $this->markTestSkipped('Test code involving PHP 7.3 heredocs will not tokenize correctly on PHP < 7.3');
+        }
+
+        $file = $this->sniffFile(__DIR__ . '/' . self::TEST_FILE_PHP73HEREDOCS, '5.4');
+        $this->assertNoViolation($file, $line);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @see testNoFalsePositivesInIndentedHeredoc()
+     *
+     * @return array
+     */
+    public function dataNoFalsePositivesInIndentedHeredoc()
+    {
+        $cases = [];
+
+        // No errors expected on the first 15 lines.
+        for ($line = 1; $line <= 15; $line++) {
+            $cases[] = [$line];
+        }
+
+        return $cases;
     }
 
 
     /**
      * Verify no notices are thrown at all.
      *
+     * @dataProvider dataTestFiles
+     *
+     * @param string $testFile File name for the test case file to use.
+     *
      * @return void
      */
-    public function testNoViolationsInFileOnValidVersion()
+    public function testNoViolationsInFileOnValidVersion($testFile)
     {
-        $file = $this->sniffFile(__FILE__, '5.3');
+        if ($testFile === self::TEST_FILE_PHP73HEREDOCS && \PHP_VERSION_ID < 70300) {
+            $this->markTestSkipped('Test code involving PHP 7.3 heredocs will not tokenize correctly on PHP < 7.3');
+        }
+
+        $file = $this->sniffFile(__DIR__ . '/' . $testFile, '5.3');
         $this->assertNoViolation($file);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @return array
+     */
+    public function dataTestFiles()
+    {
+        return [
+            [self::TEST_FILE],
+            [self::TEST_FILE_PHP73HEREDOCS],
+        ];
     }
 }
