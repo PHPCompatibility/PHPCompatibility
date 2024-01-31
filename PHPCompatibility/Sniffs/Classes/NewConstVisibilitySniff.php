3a8aa89281f065b6364f03f650f358504ea73d90
jrfnl <jrfnl@users.noreply.github.com>
2024-01-31 16:40:11 +0100

    Classes/NewConstVisibility: use new PHPCSUtils 1.1.0 Constants class
    As the feature supported for class constant declarations in PHP in recent years have become more complex, PHPCSUtils 1.1.0 introduces a new `PHPCSUtils\Utils\Constants::getProperties()` method to retrieve the relevant information about a class constant declaration based on a `T_CONST` token.
    
    Advantages of using the new method:
    * Instead of having to update individual sniffs for new `const` related PHP features, updates now only have to be made in one place (PHPCSUtils).
    * The results of the method are cached, so using the method should have a small, positive impact on performance if multiple sniffs each examine the same `T_CONST` token.
    
    This commit starts using this new function in the `PHPCompatibility.Classes.NewConstVisibility` sniff.

--- PHPCompatibility/Sniffs/Classes/NewConstVisibilitySniff.php
+++ PHPCompatibility/Sniffs/Classes/NewConstVisibilitySniff.php
@@ -13,8 +13,8 @@ namespace PHPCompatibility\Sniffs\Classes;
 use PHPCompatibility\Helpers\ScannedCode;
 use PHPCompatibility\Sniff;
 use PHP_CodeSniffer\Files\File;
-use PHP_CodeSniffer\Util\Tokens;
-use PHPCSUtils\Utils\Scopes;
+use PHPCSUtils\Exceptions\ValueError;
+use PHPCSUtils\Utils\Constants;
 
 /**
  * Visibility for class constants is available since PHP 7.1.
@@ -58,29 +58,23 @@ class NewConstVisibilitySniff extends Sniff
             return;
         }
 
-        $tokens = $phpcsFile->getTokens();
-        $skip   = Tokens::$emptyTokens;
-        $skip[] = \T_FINAL;
-
-        $prevToken = $phpcsFile->findPrevious($skip, ($stackPtr - 1), null, true, null, true);
-
-        // Is the previous token a visibility indicator ?
-        if ($prevToken === false || isset(Tokens::$scopeModifiers[$tokens[$prevToken]['code']]) === false) {
+        try {
+            $properties = Constants::getProperties($phpcsFile, $stackPtr);
+        } catch (ValueError $e) {
+            // Not an OO constant or parse error.
             return;
         }
 
-        // Is this a class constant ?
-        if (Scopes::isOOConstant($phpcsFile, $stackPtr) === false) {
-            // This may be a constant declaration in the global namespace with visibility,
-            // but that would throw a parse error, i.e. not our concern.
+        if ($properties['scope_token'] === false) {
+            // Not a constant with visibility explicitly set.
             return;
         }
 
         $phpcsFile->addError(
             'Visibility indicators for OO constants are not supported in PHP 7.0 or earlier. Found "%s const"',
-            $prevToken,
+            $properties['scope_token'],
             'Found',
-            [$tokens[$prevToken]['content']]
+            [$properties['scope']]
         );
     }
 }
