d6f6f1620d197913c0b1e4c622aabd3171c8581a
jrfnl <jrfnl@users.noreply.github.com>
2024-08-15 18:02:10 +0200

    PHP 8.4 | NewFunctionCallTrailingComma: handle trailing comma's for exit/die
    > . The exit (and die) language constructs now behave more like a function.
    >   They can be passed liked callables, are affected by the strict_types
    >   declare statement, and now perform the usual type coercions instead of
    >   casting any non-integer value to a string.
    >   As such, passing invalid types to exit/die may now result in a TypeError
    >   being thrown.
    >   RFC: https://wiki.php.net/rfc/exit-as-function
    
    This commit adds detection of the use of trailing comma's in `exit()`/`die()` to the `NewFunctionCallTrailingComma` sniff.
    
    Includes tests.
    
    Refs:
    * https://wiki.php.net/rfc/exit-as-function
    * https://github.com/php/php-src/blob/8853cf3ae950a1658054f286117bc8f77f724f00/UPGRADING#L40-L46
    * php/php-src 13486
    * https://github.com/php/php-src/commit/a79c70f5748743b1e22002a8ed119b9eca7557b5
    
    Related to 1731

--- PHPCompatibility/Sniffs/Syntax/NewFunctionCallTrailingCommaSniff.php
+++ PHPCompatibility/Sniffs/Syntax/NewFunctionCallTrailingCommaSniff.php
@@ -19,10 +19,15 @@ use PHPCSUtils\Tokens\Collections;
 /**
  * Detect trailing commas in function calls, `isset()` and `unset()` as allowed since PHP 7.3.
  *
+ * As of PHP 8.4, exit/die are now treated as function calls, which means they now also support
+ * trailing commas.
+ *
  * PHP version 7.3
+ * PHP version 8.4
  *
  * @link https://www.php.net/manual/en/migration73.new-features.php#migration73.new-features.core.trailing-commas
  * @link https://wiki.php.net/rfc/trailing-comma-function-calls
+ * @link https://wiki.php.net/rfc/exit-as-function
  *
  * @since 8.2.0
  * @since 9.0.0  Renamed from `NewTrailingCommaSniff` to `NewFunctionCallTrailingCommaSniff`.
@@ -60,11 +65,18 @@ final class NewFunctionCallTrailingCommaSniff extends Sniff
      */
     public function process(File $phpcsFile, $stackPtr)
     {
-        if (ScannedCode::shouldRunOnOrBelow('7.2') === false) {
+        if (ScannedCode::shouldRunOnOrBelow('8.3') === false) {
+            // Trailing commas are supported in both function calls + exit on PHP 8.4 and higher.
             return;
         }
 
         $tokens = $phpcsFile->getTokens();
+        if ($tokens[$stackPtr]['code'] !== \T_EXIT
+            && ScannedCode::shouldRunOnOrBelow('7.2') === false
+        ) {
+            // Not an exit expression, so we only need run for PHP 7.2 and lower.
+            return;
+        }
 
         $nextNonEmpty = $phpcsFile->findNext(Tokens::$emptyTokens, ($stackPtr + 1), null, true);
         if ($tokens[$nextNonEmpty]['code'] !== \T_OPEN_PARENTHESIS
@@ -88,7 +100,8 @@ final class NewFunctionCallTrailingCommaSniff extends Sniff
             return;
         }
 
-        $data = [];
+        $message = 'Trailing commas are not allowed in %s in PHP 7.2 or earlier';
+        $data    = [];
         switch ($tokens[$stackPtr]['code']) {
             case \T_ISSET:
                 $data[]    = 'calls to isset()';
@@ -100,17 +113,18 @@ final class NewFunctionCallTrailingCommaSniff extends Sniff
                 $errorCode = 'FoundInUnset';
                 break;
 
+            case \T_EXIT:
+                $message   = 'Trailing commas are not allowed in %s in PHP 8.3 or earlier';
+                $data[]    = 'calls to exit() or die()';
+                $errorCode = 'FoundInExitDie';
+                break;
+
             default:
                 $data[]    = 'function calls';
                 $errorCode = 'FoundInFunctionCall';
                 break;
         }
 
-        $phpcsFile->addError(
-            'Trailing commas are not allowed in %s in PHP 7.2 or earlier',
-            $lastInParenthesis,
-            $errorCode,
-            $data
-        );
+        $phpcsFile->addError($message, $lastInParenthesis, $errorCode, $data);
     }
 }
