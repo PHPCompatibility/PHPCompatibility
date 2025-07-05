e7b10e89ba5015e92b94d6f73eb383a7c181bf11
jrfnl <jrfnl@users.noreply.github.com>
2025-07-06 01:32:24 +0200

    NewLanguageConstructs: improve error messages
    ... by using proper capitalization.

--- PHPCompatibility/Sniffs/LanguageConstructs/NewLanguageConstructsSniff.php
+++ PHPCompatibility/Sniffs/LanguageConstructs/NewLanguageConstructsSniff.php
@@ -47,12 +47,12 @@ class NewLanguageConstructsSniff extends Sniff
         'T_NS_SEPARATOR' => [
             '5.2'         => false,
             '5.3'         => true,
-            'description' => 'the \ operator (for namespaces)',
+            'description' => 'The \ operator (for namespaces)',
         ],
         'T_ELLIPSIS' => [
             '5.5'         => false,
             '5.6'         => true,
-            'description' => 'the ... spread operator',
+            'description' => 'The ... spread operator',
         ],
     ];
 
