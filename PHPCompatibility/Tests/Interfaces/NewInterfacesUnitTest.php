7814967ae324e059fe56028946e59cfe27a908b1
jrfnl <jrfnl@users.noreply.github.com>
2025-07-08 18:00:17 +0200

    PHP 8.3 | Classes/NewInterfaces: add test with readonly anonymous class
    Already handled correctly by the sniff, just safeguarding this.

--- PHPCompatibility/Tests/Interfaces/NewInterfacesUnitTest.php
+++ PHPCompatibility/Tests/Interfaces/NewInterfacesUnitTest.php
@@ -85,7 +85,7 @@ class NewInterfacesUnitTest extends BaseSniffTestCase
             ['DOMParentNode', '7.4', [196, 204, 210], '8.0'],
             ['UnitEnum', '8.0', [198, 211], '8.1'],
             ['BackedEnum', '8.0', [198, 211], '8.1'],
-            ['Random\Engine', '8.1', [200], '8.2'],
+            ['Random\Engine', '8.1', [200, 216], '8.2'],
             ['Random\CryptoSafeEngine', '8.1', [200], '8.2'],
         ];
     }
