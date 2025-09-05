de7863bddad74846580a0b0b43746c2b8fd94da7
jrfnl <jrfnl@users.noreply.github.com>
2025-09-05 21:26:11 +0200

    TokenGroup::isNumber(): minor simplification
    As of PHPCS, namespaced names use the tokenization as per PHP 8.0. This also means that we no longer need to skip over a `T_NS_SEPARATOR` token for a namespaced name, as if it were, it would be tokenized differently.

--- PHPCompatibility/Helpers/TokenGroup.php
+++ PHPCompatibility/Helpers/TokenGroup.php
@@ -156,9 +156,7 @@ final class TokenGroup
             return false;
         }
 
-        $skipOver                  = Tokens::$emptyTokens;
-        $skipOver[\T_NS_SEPARATOR] = \T_NS_SEPARATOR;
-        $nextNonEmpty              = $phpcsFile->findNext($skipOver, $start, $searchEnd, true);
+        $nextNonEmpty = $phpcsFile->findNext(Tokens::$emptyTokens, $start, $searchEnd, true);
         while ($nextNonEmpty !== false
             && ($tokens[$nextNonEmpty]['code'] === \T_PLUS
             || $tokens[$nextNonEmpty]['code'] === \T_MINUS)
@@ -167,7 +165,7 @@ final class TokenGroup
                 $negativeNumber = ($negativeNumber === false) ? true : false;
             }
 
-            $nextNonEmpty = $phpcsFile->findNext($skipOver, ($nextNonEmpty + 1), $searchEnd, true);
+            $nextNonEmpty = $phpcsFile->findNext(Tokens::$emptyTokens, ($nextNonEmpty + 1), $searchEnd, true);
         }
 
         if ($nextNonEmpty === false || isset($maybeValidTokens[$tokens[$nextNonEmpty]['code']]) === false) {
