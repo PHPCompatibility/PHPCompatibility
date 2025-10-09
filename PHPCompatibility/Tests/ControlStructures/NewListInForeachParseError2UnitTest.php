ea915d50d44cf9017ce5d7d128ab80548fc3f5b4
jrfnl <jrfnl@users.noreply.github.com>
2025-10-09 19:04:21 +0200

    ControlStructures/NewListInForeachUnitTest: move live coding/parse error test to separate test class

---
+++ PHPCompatibility/Tests/ControlStructures/NewListInForeachParseError2UnitTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\ControlStructures;
+
+use PHPCompatibility\Tests\BaseSniffTestCase;
+
+/**
+ * Test the NewListInForeach sniff.
+ *
+ * @group newListInForeach
+ * @group controlStructures
+ *
+ * @covers \PHPCompatibility\Sniffs\ControlStructures\NewListInForeachSniff
+ *
+ * @since 10.0.0
+ */
+final class NewListInForeachParseError2UnitTest extends BaseSniffTestCase
+{
+
+    /**
+     * Verify the sniff will silently ignore live coding.
+     *
+     * @return void
+     */
+    public function testSniffStaysSilentOnLiveCoding()
+    {
+        $file = $this->sniffFile(__FILE__, '5.4');
+        $this->assertNoViolation($file);
+    }
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '5.5');
+        $this->assertNoViolation($file);
+    }
+}
