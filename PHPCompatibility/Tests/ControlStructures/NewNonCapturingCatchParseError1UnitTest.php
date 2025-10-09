19d63df963dfd5cf25c44f35fd2fd5b3a3c77544
jrfnl <jrfnl@users.noreply.github.com>
2025-10-09 19:09:34 +0200

    ControlStructures/NewNonCapturingCatchUnitTest: move live coding/parse error test to separate test class

---
+++ PHPCompatibility/Tests/ControlStructures/NewNonCapturingCatchParseError1UnitTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\ControlStructures;
+
+use PHPCompatibility\Tests\BaseSniffTestCase;
+
+/**
+ * Test the NewNonCapturingCatch sniff.
+ *
+ * @group newNonCapturingCatch
+ * @group controlStructures
+ * @group exceptions
+ *
+ * @covers \PHPCompatibility\Sniffs\ControlStructures\NewNonCapturingCatchSniff
+ *
+ * @since 10.0.0
+ */
+final class NewNonCapturingCatchParseError1UnitTest extends BaseSniffTestCase
+{
+
+    /**
+     * Verify the sniff will silently ignore live coding.
+     *
+     * @return void
+     */
+    public function testSniffStaysSilentOnLiveCoding()
+    {
+        $file = $this->sniffFile(__FILE__, '7.4');
+        $this->assertNoViolation($file);
+    }
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '8.0');
+        $this->assertNoViolation($file);
+    }
+}
