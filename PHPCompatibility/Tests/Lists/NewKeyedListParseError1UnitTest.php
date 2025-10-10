a73d8a7919848834e7d64c3d0f60eb9c46b4763f
jrfnl <jrfnl@users.noreply.github.com>
2025-10-10 02:23:40 +0200

    Lists/NewKeyedListUnitTest: move live coding/parse error test to separate test class

---
+++ PHPCompatibility/Tests/Lists/NewKeyedListParseError1UnitTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\Lists;
+
+use PHPCompatibility\Tests\BaseSniffTestCase;
+
+/**
+ * Test the NewKeyedList sniff.
+ *
+ * @group newKeyedList
+ * @group lists
+ *
+ * @covers \PHPCompatibility\Sniffs\Lists\NewKeyedListSniff
+ *
+ * @since 10.0.0
+ */
+final class NewKeyedListParseError1UnitTest extends BaseSniffTestCase
+{
+
+    /**
+     * Verify the sniff will silently ignore live coding.
+     *
+     * @return void
+     */
+    public function testSniffStaysSilentOnLiveCoding()
+    {
+        $file = $this->sniffFile(__FILE__, '7.0');
+        $this->assertNoViolation($file);
+    }
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '7.1');
+        $this->assertNoViolation($file);
+    }
+}
