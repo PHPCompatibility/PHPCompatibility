977b3fa3e53a0e589f42e3dcae1fb657e6819908
Anna Filina <afilina@gmail.com>
2025-12-04 12:24:19 -0500

    Extract parse error test into a separate TestCase

---
+++ PHPCompatibility/Tests/FunctionNameRestrictions/RemovedMagicMethodsParseError1UnitTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * PHPCompatibility, an external standard for PHP_CodeSniffer.
+ *
+ * @package   PHPCompatibility
+ * @copyright 2012-2020 PHPCompatibility Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCompatibility/PHPCompatibility
+ */
+
+namespace PHPCompatibility\Tests\FunctionNameRestrictions;
+
+use PHPCompatibility\Tests\BaseSniffTestCase;
+
+/**
+ * Test the RemovedMagicMethodsSniff sniff.
+ *
+ * @group removedMagicMethods
+ * @group functionNameRestrictions
+ *
+ * @covers \PHPCompatibility\Sniffs\FunctionNameRestrictions\RemovedMagicMethodsSniff
+ *
+ * @since 10.0.0
+ */
+final class RemovedMagicMethodsParseError1UnitTest extends BaseSniffTestCase
+{
+
+    /**
+     * Verify the sniff will silently ignore live coding.
+     *
+     * @return void
+     */
+    public function testSniffStaysSilentOnLiveCoding()
+    {
+        $file = $this->sniffFile(__FILE__, '8.2');
+        $this->assertNoViolation($file);
+    }
+
+    /**
+     * Verify no notices are thrown at all.
+     *
+     * @return void
+     */
+    public function testNoViolationsInFileOnValidVersion()
+    {
+        $file = $this->sniffFile(__FILE__, '8.3');
+        $this->assertNoViolation($file);
+    }
+}
