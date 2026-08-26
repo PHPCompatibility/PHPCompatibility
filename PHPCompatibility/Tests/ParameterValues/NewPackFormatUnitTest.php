7a6f55ecce0e33e4fb5944eed38d91f910dc76c1
jrfnl <jrfnl@users.noreply.github.com>
2026-08-26 04:57:53 +0200

    PHP 8.6 | ParameterValues/NewPackFormat: detect new "endianness modifiers on integers" codes (RFC)
    > - Standard:
    >   . pack() and unpack() now accept the "<" and ">" endianness modifiers on
    >     the signed and unsigned integer format codes.
    >     RFC: https://wiki.php.net/rfc/pack-unpack-endianness-signed-integers-support
    
    This commit updates the existing `NewPackFormat` sniff to allow detecting use of the new modifiers.
    
    Mind: as per the examples in the RFC, a `$format` parameter can easily contain multiple uses of the new modifiers in the format string.
    As things were, the sniff would only flag the first instance of a code found in the format string.
    To allow people to make all necessary fixes in one go, it is important to prevent one error hiding behind another.
    To that end, the sniff now uses `preg_match_all()` instead of `preg_match()` and loops over all matches to flag all instances of found incompatible codes.
    
    Refs:
    * https://wiki.php.net/rfc/pack-unpack-endianness-signed-integers-support
    * https://github.com/php/php-src/blob/75ad0885c9acd7735442d7de3c038fb59aa147c8/UPGRADING#L433-L436
    * php/php-src 21074
    * https://github.com/php/php-src/commit/02229e2337aa6188d2eaa01d586b33a69fd39218
    
    Related to 2052

--- PHPCompatibility/Tests/ParameterValues/NewPackFormatUnitTest.php
+++ PHPCompatibility/Tests/ParameterValues/NewPackFormatUnitTest.php
@@ -56,11 +56,11 @@ final class NewPackFormatUnitTest extends BaseSniffTestCase
     }
 
     /**
-     * dataNewPackFormat
+     * Data provider.
      *
      * @see testNewPackFormat()
      *
-     * @return array
+     * @return array<array<int|string>>
      */
     public static function dataNewPackFormat()
     {
@@ -78,12 +78,29 @@ final class NewPackFormatUnitTest extends BaseSniffTestCase
             [18, 'J', 'pack', '5.6', '7.1', '5.6.2'], // OK version set to beyond last error.
             [18, 'E', 'pack', '7.0', '7.1', '7.0.14'],
             [20, 'P', 'unpack', '5.6', '7.0', '5.6.2'],
+
+            // Endianness modifiers on integers.
+            [47, 's<', 'pack', '8.5', '8.6'],
+            [47, 'l<', 'pack', '8.5', '8.6'],
+            [47, 'q<', 'pack', '8.5', '8.6'],
+            [48, 's>', 'pack', '8.5', '8.6'],
+            [48, 'l>', 'pack', '8.5', '8.6'],
+            [48, 'q>', 'pack', '8.5', '8.6'],
+            [49, 'S<', 'pack', '8.5', '8.6'],
+            [49, 'L>', 'pack', '8.5', '8.6'],
+            [49, 'Q<', 'pack', '8.5', '8.6'],
+            [50, 's<', 'pack', '8.5', '8.6'],
+            [50, 'l>', 'pack', '8.5', '8.6'],
+            [51, 's<', 'unpack', '8.5', '8.6'],
+            [51, 'l<', 'unpack', '8.5', '8.6'],
+            [52, 'S>', 'unpack', '8.5', '8.6'],
+            [52, 'L<', 'unpack', '8.5', '8.6'],
         ];
     }
 
 
     /**
-     * testNoFalsePositives
+     * Test that there are no false positives.
      *
      * @dataProvider dataNoFalsePositives
      *
@@ -102,7 +119,7 @@ final class NewPackFormatUnitTest extends BaseSniffTestCase
      *
      * @see testNoFalsePositives()
      *
-     * @return array
+     * @return array<array<int|string>>
      */
     public static function dataNoFalsePositives()
     {
@@ -117,6 +134,10 @@ final class NewPackFormatUnitTest extends BaseSniffTestCase
             $data[] = [$line];
         }
 
+        for ($line = 56; $line <= 59; $line++) {
+            $data[] = [$line];
+        }
+
         return $data;
     }
 
@@ -128,7 +149,7 @@ final class NewPackFormatUnitTest extends BaseSniffTestCase
      */
     public function testNoViolationsInFileOnValidVersion()
     {
-        $file = $this->sniffFile(__FILE__, '7.1');
+        $file = $this->sniffFile(__FILE__, '8.6');
         $this->assertNoViolation($file);
     }
 }
